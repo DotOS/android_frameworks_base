8578817f6217065aceb5fed751279c827b8396ad
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    Quick settings: Add Haptic Feedback to tiles [1/2]
    mydongistiny: updated for oreo
    
    Change-Id: Ibf222b4a7badf7e7d15fefb192ab2d51764c36a2
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12464,6 +12464,12 @@ public final class Settings {
         private static final Validator OMNI_QS_QUICKBAR_COLUMNS_VALIDATOR =
                 ANY_INTEGER_VALIDATOR;
 
+        /** Whether to vibrate when quick settings tile is pressed.
+          *
+          * @hide
+          */
+          public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
