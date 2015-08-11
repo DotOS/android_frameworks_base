4dc741591b1ae3c63a543d6636b165a2bb1b768e
Tapas Kumar Kundu <tkundu@codeaurora.org>
2015-08-11 16:27:39 -0700

    ActivityTrigger: New class to invoke when activity starts/resumes
    This changes is combination of following commits:
    1) Author: David Ng <dave@codeaurora.org>
       Proprietary client library needs to know activity name. Example:app
       trigger
       Change-Id: I3078ef90d0b1846cdbbf9eaefd68544263936040
    2) Author: Rajulu Ponnada <rajulu@codeaurora.org>
       Change activity trigger api to include activity flags which can be
       changed if required in vendor specific library.
       Change-Id: Icfaab21fd4c5a6290f226a338870b9031d32b498
    3) Author: Rajulu Ponnada <rajulu@codeaurora.org>
       support for opening/loading multiple dynamic libraries when apps'
       activity start/resume invoked
       Change-Id: Icf42e426870ee634920c2288e4075699c472d3a6
    4) Author: Xin Qi <xqi@codeaurora.org>
       GetStringUTFChars() does not release memory untill
       ReleaseStringUTFChars() is called and causes memory leak. This
       change uses ReleaseStringUTFChars() to release unused buffer.
       Change-Id: If7f27ea35f5a1d1dd7c98fbe5f92729ab9ba0257
    
    Change-Id: I089f1669e5711efcabbf1985d80b48c6e92893c6
    Signed-off-by: Joe Maples <joe@frap129.org>

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -369,6 +369,8 @@ final class ActivityStack {
 
     final Handler mHandler;
 
+    static final ActivityTrigger mActivityTrigger = new ActivityTrigger();
+
     final class ActivityStackHandler extends Handler {
 
         ActivityStackHandler(Looper looper) {
@@ -2283,7 +2285,10 @@ final class ActivityStack {
 
         if (DEBUG_SWITCH) Slog.v(TAG_SWITCH, "Resuming " + next);
 
-        // If we are currently pausing an activity, then don't do anything until that is done.
+        mActivityTrigger.activityResumeTrigger(next.intent);
+
+        // If we are currently pausing an activity, then don't do anything
+        // until that is done.
         if (!mStackSupervisor.allPausedActivitiesComplete()) {
             if (DEBUG_SWITCH || DEBUG_PAUSE || DEBUG_STATES) Slog.v(TAG_PAUSE,
                     "resumeTopActivityLocked: Skip resume: some activity pausing.");
@@ -2813,6 +2818,7 @@ final class ActivityStack {
         task.setFrontOfTask();
 
         r.putInHistory();
+        r.info.flags = mActivityTrigger.activityStartTrigger(r.intent, r.info.flags);
         if (!isHomeStack() || numActivities() > 0) {
             // We want to show the starting preview window if we are
             // switching to a new task, or the next activity's process is
