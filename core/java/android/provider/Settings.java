a49a4f7ea2983b2383ea0d910f3cf54352c92f4b
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2015-08-18 12:27:39 +0530

    SystemUI: Add visualizer feature
    This is a squash of the following commits:
    
    thanks rajesh for find
    
    Author: Utkarsh Gupta <utkarsh.eminem@gmail.com>
    Date:   Tue Aug 18 12:27:39 2015 +0530
    
        SystemUI: use new visualizer from Eleven
    
        Change-Id: I4441440868cac71caa32c91a0875f00a630fe596
        Signed-off-by: Utkarsh Gupta <utkarsh.eminem@gmail.com>
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Mikalacki Sava <mikalackis@gmail.com>
    Date:   Thu Sep 3 23:10:35 2015 +0200
    
        SystemUI: fix NPE crash when Visualizer is not initialized
    
        Change-Id: Ic3774011acfae0312f1642d88c7f5bf2abc9f5a7
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Fri Sep 4 15:55:39 2015 -0700
    
        SystemUI: hide visualizer when keyguard is occluded
    
        Change-Id: Ia943fd829f7a379fee0f657bb08c34b8c6f36caa
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Jan 7 19:27:38 2016 +0100
    
        SystemUI: Make lockscreen visualizer battery friendly
    
        In case of offloaded playback, the device is allowed to sleep.
        However, if the lockscreen visualizer is enabled, the device
        hardly enters deep sleep, even if the screen is off. To prevent
        this, enable the visualizer only when the lockscreen is shown.
    
        Change-Id: I484270694b734c9b53e61a7c0ad74391b54fe8cd
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Mar 17 09:18:56 2016 -0700
    
        SystemUI: more consistent visualizer logic
    
        - only call setVisible from screen on and screen off methods
        - eagerly unregister self to make sure we don't get added twice as a
          listener
        - Add some debugging under the DEBUG flag
    
        Change-Id: Iada13058f87d4c8d8c8b60f6eebf055652ff2c8d
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Mon Apr 4 15:29:34 2016 -0700
    
        SystemUI: show visualizer in shade-locked view
    
        This brings back the behavior we had in 12.1 - visualizer would still be
        visible in the shade locked state.
    
        Also with the following improvements:
            * use less alpha
            * always attach/detach in a background thread (Async task)
            * use the statusbar state instead keyguard view state listener
    
        Change-Id: I6b8a57f9f0bba6ba0591de0cb1f94a0eb904cc1c
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Dan Pasanen <invisiblek@cyanogenmod.org>
    Date:   Wed May 18 20:27:47 2016 -0500
    
        Visualizer: Let visualizer fill the entire available screen
    
        * This will allow it to be more dynamic (landscape lock screen for instance)
    
        Change-Id: I2ffd6b0ca8832812faaa4cc0ec4cdd6071fbec8c
    
    Author: Matthias Yzusqui <myzb.dev@gmail.com>
    Date:   Wed Sep 21 15:25:00 2016 +0200
    
        SystemUI: fix navbar drawing glitch
    
        Change-Id: I58e255517d35675c043061f2edb398dab0d16ba8
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Oct 7 08:56:25 2016 +0800
    
        SystemUI: Use Tuner API for CM settings
    
        Get rid of all the excess code by implementing TunerService.Tunable
        and observing any changes made to the settings through it.
    
        Remove UserContentObserver as the Tuner API handles user switches.
    
        Also remove some unused imports that were introduced in earlier
        CM commits, but were never removed since.
    
        Change-Id: Iecafafabdaec82b3b3c72293bea865de48f0e90a
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Sat Dec 2 15:10:28 2017 +0000
    
        SystemUI: Forward-port visualizer feature to O
    
    Change-Id: I7a8497eaac9129f14b328e070c936e135fa85db6
    Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8241,6 +8241,13 @@ public final class Settings {
         public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
 
         /**
+         * Lockscreen Visualizer
+         *
+         * @hide
+         */
+        public static final String LOCKSCREEN_VISUALIZER_ENABLED = "lockscreen_visualizer";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
