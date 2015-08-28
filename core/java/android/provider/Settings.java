a57783468a6770447951f38552990905c6e98561
LorDClockaN <davor@losinj.com>
2015-08-28 20:59:06 +0200

    Toast icon switch [1/2]
    Change-Id: I24968141051805ee8c9bd5819e5f801c0dd78ef3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5749,6 +5749,13 @@ public final class Settings {
         public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
 
         /**
+         * Toast icon
+         * @hide
+         */
+        @Readable
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
