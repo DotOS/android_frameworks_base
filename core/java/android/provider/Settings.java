6c69b9db978e9382d5b1b353ec9ee973a4b3998d
beanstown106 <nbenis106@gmail.com>
2015-10-23 21:07:35 -0400

    SystemUI: Allow to enable Lockscreen Media Art
    This is a squash of the following commits:
    
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Fri Oct 23 21:07:35 2015 -0400
    
        SystemUI: Allow to enable Lockscreen Media Art
    
        Change-Id: Id93e115649df5d6dc680101a8c852644b217c28a
    
    Author: Ezio Lacandia Bijelkic <brabus84@gmail.com>
    Date:   Fri Nov 20 00:41:04 2020 +0200
    
        SystemUI: Hide lockscreen media art if media is not playing
    
        it's annoying to get the artwork even if the notification gets
        swiped out and media is paused, because the media session is still alive
    
        Change-Id: Ie65932127e47c463ef9a8b763298fc4dae20f8ae
    
    Change-Id: Idd3033b52b4aa1cf07eb694ea0c9c1c934248f7c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10997,6 +10997,13 @@ public final class Settings {
         public static final String SYSTEM_BLACK_THEME = "system_black_theme";
 
         /**
+         * Whether to show media art on lockscreen
+         * Boolean setting. 0 = off, 1 = on.
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
