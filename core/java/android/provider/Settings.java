936c9602e7d9b6d0be0c69824d76227ba95b0b4c
Roman Birg <roman@cyngn.com>
2015-11-11 15:01:41 -0500

    Add Quick Settings Pulldown options
    PureNexus Edits:
    *add always quick pulldown option
    *fix for double finger pulldown
    
    Change-Id: Ie556170bcb7b21626fab8d66baf68521b6430f9d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4182,12 +4182,16 @@ public final class Settings {
         public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
 
         /**
-
-        /**
          * Whether to show the battery info on the lockscreen while charging
          * @hide
          */
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+        
+       /**
+         * Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
