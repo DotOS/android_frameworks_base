4033003ebda84b59a9478e68dfe17431377d7f8c
Adnan Begovic <adnan@cyngn.com>
2015-12-11 16:16:02 -0800

    SystemUI: Readd AmbientDisplayTile.
    Original change squashed with:
    
      From: Michael Bestas <mkbestas@lineageos.org>
      Date: Sun, 17 Sep 2017 22:51:15 +0300
    
        SystemUI: Hide ambient display tile if device does not support it
    
        * Add a check similar to the Settings app
    
        Change-Id: I603cd28b8dd1fadff9ffcfb6953c7b72514b5857
    
    Change-Id: Ie7b93ba0f87d505206b5d60f193c2234d83d7eac

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -24,6 +24,7 @@ import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
+import com.android.systemui.qs.tiles.AmbientDisplayTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
@@ -95,6 +96,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("navigation")) return new NavigationBarTile(mHost);
         else if (tileSpec.equals("hwkeys")) return  new HWKeysTile(mHost);
 		else if (tileSpec.equals("caffeine")) return new CaffeineTile(mHost);
+		else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
