6beb37183d23b5235002cae7efa01a235b75dfe2
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    base: Fingerprint authentication & error vibration [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>
    Change-Id: I365cd1d8f648ee957b2c8bb6bae92dc3a735cce0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5103,6 +5103,18 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Whether to enable or disable vibration on succesful fingerprint auth
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
+         * Whether to enable or disable vibration on fingerprint auth error
+         * @hide
+         */
+        public static final String FINGERPRINT_ERROR_VIB = "fingerprint_error_vib";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
