e69f6193df9bf6cc9605de5e8b2d9922c727824d
Roman Birg <romanbirg@gmail.com>
2016-10-23 06:43:28 +0200

    Add isPackageInstalled boolean
    Change-Id: Ibadea83b68c012c08e47c3622ba96a37c8ddf1b9
    Signed-off-by: Joe Maples <joe@frap129.org>

--- core/java/com/android/internal/util/dot/DotUtils.java
+++ core/java/com/android/internal/util/dot/DotUtils.java
@@ -17,6 +17,7 @@
 package com.android.internal.util.dot;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
@@ -45,6 +46,25 @@ public class DotUtils {
        } catch (NameNotFoundException e) {
            return false;
        }
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 
     public static boolean deviceSupportsFlashLight(Context context) {
