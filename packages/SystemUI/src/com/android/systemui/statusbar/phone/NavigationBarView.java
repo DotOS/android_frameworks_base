6cccd45bb348747f9bbfea1c2ea905566a07f96b
bryan2894 <bryan2894@gmail.com>
2016-11-10 01:37:27 -0500

    SystemUI: add more buttons to NavBar Tuner
    @mydongistiny:
     -Updated for 8.1 with N Tuner
     -Fixed buttons for ButtonDispatcher
     -Added new search drawble
    Change-Id: I69ddeadb66b4ada6ff17e07258f9fd7d6f00d6e2
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -133,6 +133,7 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
     private KeyButtonDrawable mDockedIcon;
     private KeyButtonDrawable mImeIcon;
     private KeyButtonDrawable mMenuIcon;
+    private KeyButtonDrawable mSearchIcon;
     private KeyButtonDrawable mAccessibilityIcon;
     private TintedKeyButtonDrawable mRotateSuggestionIcon;
 
@@ -301,6 +302,8 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
         mButtonDispatchers.put(R.id.home, new ButtonDispatcher(R.id.home));
         mButtonDispatchers.put(R.id.recent_apps, new ButtonDispatcher(R.id.recent_apps));
         mButtonDispatchers.put(R.id.menu, new ButtonDispatcher(R.id.menu));
+        mButtonDispatchers.put(R.id.menu_always_show, new ButtonDispatcher(R.id.menu_always_show));
+        mButtonDispatchers.put(R.id.search, new ButtonDispatcher(R.id.search));
         mButtonDispatchers.put(R.id.ime_switcher, new ButtonDispatcher(R.id.ime_switcher));
         mButtonDispatchers.put(R.id.accessibility_button,
                 new ButtonDispatcher(R.id.accessibility_button));
@@ -415,6 +418,10 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
         return mButtonDispatchers.get(R.id.menu);
     }
 
+    public ButtonDispatcher getMenuAlwaysShowButton() {
+        return mButtonDispatchers.get(R.id.menu_always_show);
+    }
+
     public ButtonDispatcher getBackButton() {
         return mButtonDispatchers.get(R.id.back);
     }
@@ -425,6 +432,10 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
 
     public ButtonDispatcher getImeSwitchButton() {
         return mButtonDispatchers.get(R.id.ime_switcher);
+    }
+
+    public ButtonDispatcher getSearchButton() {
+        return mButtonDispatchers.get(R.id.search);
     }
 
     public ButtonDispatcher getAccessibilityButton() {
@@ -493,6 +504,7 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
             mBackIcon = getBackDrawable(lightContext, darkContext);
             mRecentIcon = getDrawable(lightContext, darkContext, R.drawable.ic_sysbar_recent);
             mMenuIcon = getDrawable(lightContext, darkContext, R.drawable.ic_sysbar_menu);
+            mSearchIcon = getDrawable(lightContext, darkContext, R.drawable.ic_sysbar_search_default);
 
             mAccessibilityIcon = getDrawable(lightContext, darkContext,
                     R.drawable.ic_sysbar_accessibility_button, false /* hasShadow */);
@@ -650,6 +662,8 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
         // Update menu button, visibility logic in method
         setMenuVisibility(mShowMenu, true);
         getMenuButton().setImageDrawable(mMenuIcon);
+        getSearchButton().setImageDrawable(mSearchIcon);
+        getMenuAlwaysShowButton().setImageDrawable(mMenuIcon);
 
         // Update rotate button, visibility altered by a11y button logic
         getRotateSuggestionButton().setImageDrawable(mRotateSuggestionIcon);
@@ -666,6 +680,8 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
         boolean disableRecent = mUseCarModeUi || !isOverviewEnabled();
 
         boolean disableBack = ((mDisabledFlags & View.STATUS_BAR_DISABLE_BACK) != 0) && !useAltBack;
+
+        final boolean disableSearch = ((mDisabledFlags & View.STATUS_BAR_DISABLE_SEARCH) != 0);
 
         // When screen pinning, don't hide back and home when connected service or back and
         // recents buttons when disconnected from launcher service in screen pinning mode,
@@ -700,6 +716,7 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
         getBackButton().setVisibility(disableBack      ? View.INVISIBLE : View.VISIBLE);
         getHomeButton().setVisibility(disableHome      ? View.INVISIBLE : View.VISIBLE);
         getRecentsButton().setVisibility(disableRecent ? View.INVISIBLE : View.VISIBLE);
+        getSearchButton().setVisibility(disableSearch ? View.INVISIBLE : View.VISIBLE);
     }
 
     public boolean inScreenPinning() {
@@ -825,8 +842,12 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
                 !mShowAccessibilityButton &&
                 !mShowRotateButton &&
                 ((mNavigationIconHints & StatusBarManager.NAVIGATION_HINT_IME_SHOWN) == 0);
+        final boolean shouldShowAlwaysMenu = (mNavigationIconHints &
+                StatusBarManager.NAVIGATION_HINT_IME_SHOWN) == 0;
 
         getMenuButton().setVisibility(shouldShow ? View.VISIBLE : View.INVISIBLE);
+        getMenuAlwaysShowButton().setVisibility(shouldShowAlwaysMenu ? View.VISIBLE : View.INVISIBLE);
+        getSearchButton().setVisibility(shouldShowAlwaysMenu ? View.VISIBLE : View.INVISIBLE);
     }
 
     public void setAccessibilityButtonState(final boolean visible, final boolean longClickable) {
