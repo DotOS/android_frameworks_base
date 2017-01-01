02e00e6aa81de3f58bc37d4e38487f63c24b99e7
ezio84 <brabus84@gmail.com>
2016-12-31 22:50:55 -0500

    Night Light brightness mode options [1/2]
    thanks also AOSPA guys for suggestions
    
    Change-Id: I12b4f4d0893ad85dcf5c7213fd42f8a6ba567767
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7942,6 +7942,16 @@ public final class Settings {
         public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
 
         /**
+         * Whether to set a lower brightness level when enabling night mode
+         * 0: Disabled
+         * 1: Set the brightness to a very low value
+         * 2: Set the brightness to a low value
+         * 3: Set the brightness to a medium value
+         * @hide
+         */
+        public static final String NIGHT_BRIGHTNESS_VALUE = "night_brightness_value";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
