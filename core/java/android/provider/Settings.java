a118a55666e42c507b6dc892f3c58e726899bce8
Jon Haus <haus.xda@gmail.com>
2017-01-05 00:20:06 +0100

    Status network traffic [1/2]
    -----
    abc ezio84:
    use O darkintensity api
    use arrows vectors from aosp net arrows commit (thanks @Mazda-- for this find!): https://github.com/ezio84/abc_frameworks_base/commit/ad71ae1fd09f9a7392ab2eba7b766c8b088319b4
    fix visual glitch on statusbar expand (arrows showing and quickly hiding again)
    don't show arrows when no data
    remove unit settings (KB is good)
    remove interval (set to 1sec by def) and single arrow options
    don't get settings values again when not needed
    fix padding between arrows and speed values and expose the padding value
    code cleanup
    -----
    
    Change-Id: I7ca34514c59a19534a404aa4b5958505507a33e9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4108,7 +4108,7 @@ public final class Settings {
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
          * the setting value. See an example above.
          */
-
+        // DOTEXTRAS
         /**
          * Whether to show the battery info on the lockscreen while charging
          * @hide
@@ -4177,7 +4177,18 @@ public final class Settings {
          */
         public static final String POWERMENU_LOCKSCREEN = "powermenu_lockscreen";
 
-        /**
+         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+         /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+		
+		/**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
