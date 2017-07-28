b260041735f83120538fd79463ff67bd6aac725f
Ivan Iskandar <ivan@prjkt.io>
2017-07-28 04:56:45 +0200

    base: Introduce SubstratumService
    Change-Id: I8c9f331ca7e4c68c35ea13bed587775ee4abeb30
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>
    Signed-off-by: Nicholas Chum <nicholas@prjkt.io>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7315,6 +7315,13 @@ public final class Settings {
          */
         public static final String NIGHT_BRIGHTNESS_VALUE = "night_brightness_value";
 
+	/**
+         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
         /**
          * This are the settings to be backed up.
          *
