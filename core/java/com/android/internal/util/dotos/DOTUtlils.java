30b705e94f507566d8f7651ef1ab1306c2bbea9b
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Add api to take screenshots abc: extracted from DUI add a public call to AEXUtils
    Change-Id: Ica63362647eec89152d44a50ecdb83a2663b5c0c
    Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

--- core/java/com/android/internal/util/dotos/DOTUtlils.java
+++ core/java/com/android/internal/util/dotos/DOTUtlils.java
@@ -18,6 +18,7 @@
 package com.android.internal.util.dotos;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
 import android.os.Handler;
@@ -26,6 +27,8 @@ import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
@@ -36,6 +39,9 @@ import com.android.internal.statusbar.IStatusBarService;
  * Some custom utilities
  */
 public class DOTUtils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
@@ -75,6 +81,15 @@ public class DOTUtils {
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
             }
         }, 20);
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     private static final class FireActions {
