7f34c5315fc1cb1476ec09ff338e25f5dc9786c4
kameron19921016 <kameron19921016@yahoo.com>
2017-09-15 09:13:30 +0800

    FWB: Add Volume Key Answer (1/2)
    Change-Id: I3e29b6334fcbe3da4bc66166be9df2204d60c6ae

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4323,6 +4323,12 @@ public final class Settings {
         public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 
         /**
+         * volume answer
+         * @hide
+         */
+        public static final String ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER = "call_volume_answer";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
