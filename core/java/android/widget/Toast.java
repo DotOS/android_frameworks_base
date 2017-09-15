80357b0476fb79d1044ad110c79e6b6b2449fc8a
Joe Maples <joe@frap129.org>
2017-09-15 10:43:12 -0500

    core: Materialize Toast notifications
    Toasts were never materialized by Google as they discouraged their
    use, promoting SnackBars instead. Despite this, many developers
    (including Google) continue to use toasts.
    
    This commit replaces the standard toast with a material card
    containing the app icon. Icon code was taken from 0xD34D's commit
    and modified to be unpadded and centered vertically to be inline
    with the text. It also replaces the fade animations with slide
    animations to be more material.
    
    The toast card color is themeable via the "toast_bg" color value,
    and the text color is themeable via "toast_text". The shadow of
    the card is themeable via "toast_elevation"
    
    mydongistiny: Changed background/foreground for day/night
    
    Change-Id: I47577ecb31c2f292cad3ca3e47e94f39edc6cbef
    Signed-off-by: Joe Maples <joe@frap129.org>

--- core/java/android/widget/Toast.java
+++ core/java/android/widget/Toast.java
@@ -33,6 +33,8 @@ import android.compat.annotation.UnsupportedAppUsage;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Binder;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
@@ -693,9 +695,25 @@ public class Toast {
                 // remove the old view if necessary
                 handleHide();
                 mView = mNextView;
+                Context context = mView.getContext().getApplicationContext();
                 mPresenter.show(mView, mToken, windowToken, mDuration, mGravity, mX, mY,
                         mHorizontalMargin, mVerticalMargin,
                         new CallbackBinder(getCallbacks(), mHandler));
+                String packageName = mView.getContext().getOpPackageName();
+                if (context == null) {
+                    context = mView.getContext();
+                }
+                ImageView appIcon = (ImageView) mView.findViewById(android.R.id.icon);
+                if (appIcon != null) {
+                    PackageManager pm = context.getPackageManager();
+                    Drawable icon = null;
+                    try {
+                        icon = pm.getApplicationIcon(packageName);
+                    } catch (PackageManager.NameNotFoundException e) {
+                        // nothing to do
+                    }
+                    appIcon.setImageDrawable(icon);
+                }
             }
         }
 
