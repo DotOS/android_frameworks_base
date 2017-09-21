ed8d19595d4afb4cdaafacf27e96200eac6e67ff
xyyx <xyyx@mail.ru>
2017-09-21 14:16:27 +0800

    PowerMenu: add delay to screenshot action and hide power dialog when partial screenshot
    Change-Id: If98765e53222917519ad07ab60731452917b9a41
    Signed-off-by: Mohan Cm <mohanmanjappa@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -551,12 +551,21 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
 
         @Override
         public void onPress() {
+            /* wait for the dialog box to close */
+            try {
+                 Thread.sleep(1000); //1s
+            } catch (InterruptedException ie) {}
             AEXUtils.takeScreenshot(true);
         }
 
 
         @Override
         public boolean onLongPress() {
+            mHandler.sendEmptyMessage(MESSAGE_DISMISS);
+            /* wait for the dialog box to close */
+            try {
+                 Thread.sleep(1000); //1s
+            } catch (InterruptedException ie) {}
             AEXUtils.takeScreenshot(false);
             return true;
         }
