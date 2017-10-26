5df25c46bb03be6868d9376376325a9b607933a7
samgrande <talktosayan35@gmail.com>
2017-10-26 22:20:13 +0530

    Update membar a bit more often Using the FAB clear-all was not updating the membar at all and thus giving us false readings. At one point on the OP3T it said I had less than 1GB of ram with no open apps. This fixes that issue.
    Signed-off-by: samgrande <talktosayan35@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -336,6 +336,7 @@ public class RecentsView extends FrameLayout {
             @Override
             public void onClick(View view) {
                 EventBus.getDefault().send(new ToggleRecentsEvent());
+                 updateMemoryStatus();
             }
         });
 
