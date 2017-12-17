2bd9a699a6f4788d37785dc9db6d5f64fd4d8a00
Kshitij Gupta <kshitijgm@gmail.com>
2017-12-17 22:17:58 +0000

    base: overlays: Refactor sysui overlay to system [1/2]
    - We are theming more than just SystemUI here.
      Refactor sysui to system in favour of Dark UI.
    
    Change-Id: Ie5e92f343142df277c2481fde0fec15a046d1651
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3019,7 +3019,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     public boolean isUsingDarkTheme() {
         OverlayInfo themeInfo = null;
         try {
-            themeInfo = mOverlayManager.getOverlayInfo("com.android.systemui.theme.dark",
+            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.dark",
                     mCurrentUserId);
         } catch (RemoteException e) {
             e.printStackTrace();
@@ -4979,7 +4979,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         if (isUsingDarkTheme() != useDarkTheme) {
             try {
-                mOverlayManager.setEnabled("com.android.systemui.theme.dark",
+                mOverlayManager.setEnabled("com.android.system.theme.dark",
                         useDarkTheme, mCurrentUserId);
             } catch (RemoteException e) {
                 Log.w(TAG, "Can't change theme", e);
