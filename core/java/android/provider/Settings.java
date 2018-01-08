499fa02e55c4a9ab55dcd1de573470f86d44d879
maxwen <max.weninger@gmail.com>
2018-01-08 02:52:58 +0100

    Screenshot tile: persist screenshot type after reboot
    save state set in qs tile into settings and use that
    after boot
    
    Change-Id: Ieca6148862386cef01767d43938bb803aab282cb
    Signed-off-by: Mohan Cm <mohanmanjappa@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4433,6 +4433,14 @@ public final class Settings {
         public static final String ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER = "call_volume_answer";
 
         /**
+         * 0 - fullscreen
+         * 1 - partial
+         * @hide
+         */
+        public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
+
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
