e4d5bc9e2baa3d344d44c44f8eca301312e24483
ezio84 <brabus84@gmail.com>
2018-01-12 10:54:32 +0100

    Multi ScreenRecord tile, blinking stop button, screenrecord service improvements [1/2]
    single tap to toggle quality:
    - 480*800 resolution and 1.5Mbps
    - default resolution (720p) and 4Mbps
    - default resolution (720p) and 8Mbps
    long press to start recording
    
    Show also a blinking red button at bottom right of the screen
    that can be tapped to stop the recording instead of using
    the notification.
    
    Also improve the screenrecord service and be sure
    to stop it when we are done.
    
    part 2/2 is in framework/av tree, it just sets max recording
    duration to 30 minutes instead of 3
    
    Change-Id: I9a0161e6ddafb721421585c7f16bba4b30319425
    Signed-off-by: Mohan Cm <mohanmanjappa@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7737,4 +7737,9 @@ public class WindowManagerService extends IWindowManager.Stub
             w.setForceHideNonSystemOverlayWindowIfNeeded(hideSystemAlertWindows);
         }, false /* traverseTopToBottom */);
     }
+
+    @Override
+    public void screenRecordAction(int mode) {
+        mPolicy.screenRecordAction(mode);
+    }
 }
