471558c20032b266fb2dd79ca5f0606a8acec1d6
Alex Cruz <du.alexcruz@gmail.com>
2018-01-25 06:27:27 -0500

    Add Gboard overlay
    Thanks to du For the concept
    
    Change-Id: I7e6500014adb5f8d23332553b7ebffcfbbfe3f9a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5453,6 +5453,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                         useDarkTheme, mCurrentUserId);
                 mOverlayManager.setEnabled("com.android.dui.theme.dark",
                         useDarkTheme, mCurrentUserId);
+		mOverlayManager.setEnabled("com.android.gboard.theme.dark",
+                        useDarkTheme, mCurrentUserId);
             } catch (RemoteException e) {
                 Log.w(TAG, "Can't change theme", e);
             }
