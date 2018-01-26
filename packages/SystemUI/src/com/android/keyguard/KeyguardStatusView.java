ce5cc37ee23b7940c346f2925b156ef9512a28c2
Alex Cruz <du.alexcruz@gmail.com>
2018-01-26 16:55:52 -0500

    Always keep clock/date & alarm visible while dozing
    When we introduced this mod in 8.1, we didn't account for ambient display
    
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/deb4cf467085459909b82a62f18be6a1041902d2
    
    Since we didn't account for AD, if you turned off these items you wouldn't see them on
    the lockscreen while the screen is on but you also wouldn't see them while dozing.
    
    This was just bad design. The user should ALWAYS see these items while dozing. If they don't
    then the user might get the impression that ambient display doesn't work. Believe me, it happened
    to me because I forgot I had those items toggled off. All you see is a black screen if there's no
    notifications.
    
    We now check for when the device is dozing/screen off and the switch respects dozing.
    
    Steps to reproduce the fail:
    - Turn on Ambient display
    - Go into Settings/Dirty Tweaks/Lockscreen items and turn off any or all the items
    - Enter doze mode
    
    Thanks to Ezio for his method to check Doze. (I modified it to check for screen off)
    
    Change-Id: Ic9e62e7f7399ed321b923de7ddb939c76f6caa2d
    Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -29,7 +29,13 @@ import android.graphics.PorterDuff;
 import android.graphics.Typeface;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.PowerManager;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.dreams.DreamService;
+import android.service.dreams.IDreamManager;
 import android.support.v4.graphics.ColorUtils;
 import android.database.ContentObserver;
 import android.graphics.drawable.Drawable;
@@ -428,13 +434,25 @@ public class KeyguardStatusView extends GridLayout implements
                 Settings.System.HIDE_LOCKSCREEN_DATE, 0, UserHandle.USER_CURRENT) == 0;
 
         mClockView = (TextClock) findViewById(R.id.clock_view);
-        mClockView.setVisibility(showClock ? View.VISIBLE : View.GONE);
-
-        mDateView.setVisibility(showDate ? View.VISIBLE : View.GONE);
         mDateView = (DateView) findViewById(R.id.date_view);
-
         mAlarmStatusView = (TextView) findViewById(R.id.alarm_status);
-        mAlarmStatusView.setVisibility(showAlarm && nextAlarm != null ? View.VISIBLE : View.GONE);
+
+        if (!isDozeMode()) {
+            mClockView.setVisibility(showClock ? View.VISIBLE : View.GONE);
+        } else {
+            mClockView.setVisibility(View.VISIBLE);
+        }
+
+        if (!isDozeMode()) {
+            mDateView.setVisibility(showDate ? View.VISIBLE : View.GONE);
+        } else {
+            mDateView.setVisibility(View.VISIBLE);
+        }
+
+        if (!isDozeMode()) {
+            mAlarmStatusView.setVisibility(showAlarm && nextAlarm != null ? View.VISIBLE : View.GONE);
+        }
+        updateDozeVisibleViews();
     }
 
     // DateFormat.getBestDateTimePattern is extremely expensive, and refresh is called often.
@@ -499,6 +517,7 @@ public class KeyguardStatusView extends GridLayout implements
         int blendedAlarmColor = ColorUtils.blendARGB(mAlarmTextColor, Color.WHITE, darkAmount);
         mAlarmStatusView.setTextColor(blendedAlarmColor);
         mAlarmStatusView.setCompoundDrawableTintList(ColorStateList.valueOf(blendedAlarmColor));
+        refresh();
     }
 
     public void setPulsing(boolean pulsing) {
@@ -509,6 +528,7 @@ public class KeyguardStatusView extends GridLayout implements
         mForcedMediaDoze =
                 reason == DozeLog.PULSE_REASON_FORCED_MEDIA_NOTIFICATION;
         updateDozeVisibleViews();
+        refresh();
     }
 
     private void updateDozeVisibleViews() {
@@ -576,5 +596,23 @@ public class KeyguardStatusView extends GridLayout implements
 
            updateSettings(false);
          }
+
+    private boolean isDozeMode() {
+        IDreamManager dreamManager = getDreamManager();
+        PowerManager pm = (PowerManager) getContext().getSystemService(Context.POWER_SERVICE);
+        try {
+            //noinspection deprecation
+            if (dreamManager != null && dreamManager.isDozing() && pm != null && !pm.isScreenOn()) {
+                return true;
+            }
+        } catch (RemoteException e) {
+            return false;
+        }
+        return false;
+    }
+
+    static IDreamManager getDreamManager() {
+        return IDreamManager.Stub.asInterface(
+                ServiceManager.checkService(DreamService.DREAM_SERVICE));
     }
 }
