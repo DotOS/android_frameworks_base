dde65fba2f607c53d5516637d0ba688bcafc3ad3
Alex Cruz <du.alexcruz@gmail.com>
2018-01-30 20:57:14 -0500

    Make powermenu lockscreen visibility configurable [1/2]
    This gives the user the option to limit how their power menu looks on
    the lockscreen while enabling all options once the device is unlocked.
    
    --Example--
    Lockscreen - Power and Reboot
    Homescreen - Power, Reboot, Advanced Reboot, Screenshot and Airplane
    
    In the past this was either all or nothing. Having a full power meny
    on the lockscreen just looks off considering that some options are only
    essential once the device is unlocked.
    
    Change-Id: I153b79c3d6dc0be9af048b160a7ac056a364d3d2

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -355,7 +355,9 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             }
 
             public boolean showDuringKeyguard() {
-                return true;
+                boolean showlocked = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_LS_AIRPLANE, 0) == 1;
+                return showlocked;
             }
 
             public boolean showBeforeProvisioning() {
@@ -371,7 +373,9 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
                 mWindowManagerFuncs, mHandler) {
 
             public boolean showDuringKeyguard() {
-                return true;
+                boolean showlocked = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_LS_ADVANCED_REBOOT, 0) == 1;
+                return showlocked;
             }
 
             public boolean showBeforeProvisioning() {
@@ -578,7 +582,9 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
 
         @Override
         public boolean showDuringKeyguard() {
-            return true;
+            boolean showlocked = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.POWERMENU_LS_REBOOT, 1) == 1;
+            return showlocked;
         }
 
         @Override
@@ -609,7 +615,9 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
 
             @Override
             public boolean showDuringKeyguard() {
-                return true;
+                boolean showlocked = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_LS_SCREENSHOT, 0) == 1;
+                return showlocked;
             }
 
             @Override
