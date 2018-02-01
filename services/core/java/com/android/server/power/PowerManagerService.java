fa13866e7a60b86dded4a44ecc55fd2429dd9659
xyyx <xyyx@mail.ru>
2018-02-01 21:36:45 +0800

    policy: introduce pocket lock * Block touch screen and keys inputs when device is in pocket. * Safe-door to disable pocket lock within long press power button if sensors do not behave correctly after PocketManager.isDeviceInPocket() returns true. * Window uses TYPE_SYSTEM_ERROR flag with ensure high priority over the lockscreen and other high priority windows. * Do not illuminate buttons when pocket lock is showing. * Disable fingerprint events and gestures when in pocket * Be friendly towards Keypress Boost
    Contributors:
    Carlo Savignano <carlosavignano@aospa.co>
    Chris Lahaye <dev@chrislahaye.com>
    Anas Karbila <anaskarbila@aospa.co>
    Alex Naidis <alex.naidis@linux.com>
    Park Ju Hyung <qkrwngud825@gmail.com>
    
    Ticket: NOUGAT-15
    
    Updated for 8.0 by @Sirhc95
    
    Change-Id: Ic9d4711455f0f7ca348a650e3f1cbbc225645b2d

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -59,6 +59,7 @@ import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.WorkSource;
+import android.pocket.PocketManager;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.service.dreams.DreamManagerInternal;
@@ -2038,6 +2039,8 @@ public final class PowerManagerService extends SystemService
                 final int screenOffTimeout = getScreenOffTimeoutLocked(sleepTimeout);
                 final int screenDimDuration = getScreenDimDurationLocked(screenOffTimeout);
                 final boolean userInactiveOverride = mUserInactiveOverrideFromWindowManager;
+                final PocketManager pocketManager = (PocketManager) mContext.getSystemService(Context.POCKET_SERVICE);
+                final boolean isDeviceInPocket = pocketManager != null && pocketManager.isDeviceInPocket();
 
                 mUserActivitySummary = 0;
                 if (mLastUserActivityTime >= mLastWakeTime) {
@@ -2046,7 +2049,7 @@ public final class PowerManagerService extends SystemService
                     if (now < nextTimeout) {
                         if (mButtonTimeoutEnabled && (userActivityValue || settingsValue)){
                             final boolean buttonPressed = mEvent == PowerManager.USER_ACTIVITY_EVENT_BUTTON;
-                            if (mButtonBacklightOnTouchOnly) {
+                            if (mButtonBacklightOnTouchOnly  && !isDeviceInPocket) {
                                 if (buttonPressed) {
                                     triggerButtonTimeoutEvent(now);
                                 }
