1c82de10f21210156917e9588f57f52419559b54
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-02-06 11:41:02 +0530

    Overlays: Update black theme
    Also fix mismerge
    
    Change-Id: I43cead85b413f993d952aebd61786922325df723

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5012,7 +5012,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mOverlayManager.setEnabled("com.android.settings.theme.black",
                         useBlackTheme, mCurrentUserId);
                 mOverlayManager.setEnabled("com.android.dui.theme.black",
-                        useDarkTheme, mCurrentUserId);
+                        useBlackTheme, mCurrentUserId);
             } catch (RemoteException e) {
                 Log.w(TAG, "Can't change theme", e);
             }
