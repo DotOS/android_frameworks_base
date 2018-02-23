ezio84/abc-n_frameworks_base

Add navbar double tap to sleep [1/2]

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFrame.java
@@ -22,13 +22,18 @@ import android.annotation.Nullable;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.GestureDetector;
 import android.widget.FrameLayout;
 
+import com.android.internal.util.abc.AbcUtils;
 import com.android.systemui.statusbar.policy.DeadZone;
 
 public class NavigationBarFrame extends FrameLayout {
 
+    private boolean mIsDoubleTapEnabled;
+
     private DeadZone mDeadZone = null;
+    private GestureDetector mNavDoubleTapToSleep;
 
     public NavigationBarFrame(@NonNull Context context) {
         super(context);
@@ -36,6 +41,14 @@ public class NavigationBarFrame extends FrameLayout {
 
     public NavigationBarFrame(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mNavDoubleTapToSleep = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                AbcUtils.switchScreenOff(context);
+                return true;
+            }
+        });
     }
 
     public NavigationBarFrame(@NonNull Context context, @Nullable AttributeSet attrs,
@@ -55,5 +68,18 @@ public class NavigationBarFrame extends FrameLayout {
             }
         }
         return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mIsDoubleTapEnabled && mNavDoubleTapToSleep != null
+                && mNavDoubleTapToSleep.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public void setDoubleTapToSleep(boolean isDoubleTapEnabled) {
+        mIsDoubleTapEnabled = isDoubleTapEnabled;
     }
 }

DotOS/android_frameworks_base

Change-Id: I9cff9bf29fd2012292bb839c6add8f1d280fc4ce
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarFrame.java
@@ -22,14 +22,21 @@ import android.annotation.Nullable;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.GestureDetector;
 import android.widget.FrameLayout;
 
+import com.android.internal.util.aospextended.AEXUtils;
 import com.android.systemui.statusbar.policy.DeadZone;
 
 public class NavigationBarFrame extends FrameLayout {
 
+    private boolean mIsDoubleTapEnabled;
+
     private DeadZone mDeadZone = null;
+
     private boolean mEnabled = false;
+
+    private GestureDetector mNavDoubleTapToSleep;
 
     public NavigationBarFrame(@NonNull Context context) {
         super(context);
@@ -37,6 +44,14 @@ public class NavigationBarFrame extends FrameLayout {
 
     public NavigationBarFrame(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mNavDoubleTapToSleep = new GestureDetector(context,
+                new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                DOTUtils.switchScreenOff(context);
+                return true;
+            }
+        });
     }
 
     public NavigationBarFrame(@NonNull Context context, @Nullable AttributeSet attrs,
@@ -61,5 +76,18 @@ public class NavigationBarFrame extends FrameLayout {
             }
         }
         return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mIsDoubleTapEnabled && mNavDoubleTapToSleep != null
+                && mNavDoubleTapToSleep.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public void setDoubleTapToSleep(boolean isDoubleTapEnabled) {
+        mIsDoubleTapEnabled = isDoubleTapEnabled;
     }
 }