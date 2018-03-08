c523b29662e15d1fdd096677012254ef97750587
Bauuuuu <abansode@outlook.com>
2018-03-08 17:15:50 +0100

    Revert "telephony: Add backwards compatibility with pre-oreo blobs (1/2)"
    This reverts commit f544f618f3b08b554c715a635fbc46799d7edeaa.

--- telephony/java/android/telephony/SubscriptionManager.java
+++ telephony/java/android/telephony/SubscriptionManager.java
@@ -1018,11 +1018,6 @@ public class SubscriptionManager {
     }
 
     /** @hide */
-    public static int getSlotId(int subId) {
-       return getSlotIndex(subId);
-    }
-
-    /** @hide */
     public static int[] getSubId(int slotIndex) {
         if (!isValidSlotIndex(slotIndex)) {
             logd("[getSubId]- fail");
@@ -1422,11 +1417,6 @@ public class SubscriptionManager {
         }
 
         return simState;
-    }
-
-    /** @hide */
-    public static int getSimStateForSlotIdx(int slotIdx) {
-        return getSimStateForSlotIndex(slotIdx);
     }
 
     /**
