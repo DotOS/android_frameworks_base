298c0d34b359319ca2144e26fac3e4088eeb2a91
maxwen <max.weninger@gmail.com>
2018-04-13 03:32:51 +0200

    base: SystemUI: stop crashing qs tiles during boot
    this triggers my ocd
    continues a579c2430f1a0bf088023b4283dcec37776c070d
    
    Change-Id: I31719b595cb50a330f26e25074243e04c06f63c1

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -169,6 +169,12 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
+        if (mWakeLock == null) {
+            return;
+        }
+        if (state.slash == null) {
+            state.slash = new SlashState();
+        }
         state.value = mWakeLock.isHeld();
         if (state.value) {
             state.label = formatValueWithRemainingTime();
