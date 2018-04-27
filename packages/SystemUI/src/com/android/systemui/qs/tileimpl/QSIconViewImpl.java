e611e5020d414452f2fa794e368e3c9de8cf53df
IacobIonut01 <paulionut2003@gmail.com>
2018-04-27 17:38:48 +0300

    QSPanel : Implement Custom Color for Tiles
    Change-Id: I7f1fb83a2b7dc4e40dadfdd9904e5e2a7cb60d16
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSIconViewImpl.java
@@ -130,9 +130,9 @@ public class QSIconViewImpl extends QSIconView {
         updateIcon(iv, state);
         if (state.disabledByPolicy) {
             iv.setColorFilter(getContext().getColor(R.color.qs_tile_disabled_color));
-        } else {
+        } /*else {
             iv.clearColorFilter();
-        }
+        }*/
         if (state.state != mState) {
             int color = getColor(state.state);
             mState = state.state;
@@ -183,7 +183,7 @@ public class QSIconViewImpl extends QSIconView {
     }
 
     public static void setTint(ImageView iv, int color) {
-        iv.setImageTintList(ColorStateList.valueOf(color));
+	iv.setColorFilter(color,android.graphics.PorterDuff.Mode.MULTIPLY);
     }
 
 
