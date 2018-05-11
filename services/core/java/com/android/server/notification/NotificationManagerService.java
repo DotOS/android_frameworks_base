178b11c479dd21270134d3c1662f5e646908da5d
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2018-05-11 14:27:10 -0400

    Led light preview: switch led off before changing the color
    on some devices this can avoid glitches
    
    Change-Id: I0a7031cf408d31cbf1f168649820bf555eb3bec8

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4127,6 +4127,7 @@ public class NotificationManagerService extends SystemService {
 
     private void forceShowLed(int color) {
         if (color != -1) {
+            mNotificationLight.turnOff();
             mNotificationLight.setColor(color);
         } else {
             mNotificationLight.turnOff();
@@ -4135,6 +4136,7 @@ public class NotificationManagerService extends SystemService {
 
     private void forcePulseLed(int color, int onTime, int offTime) {
         if (color != -1) {
+            mNotificationLight.turnOff();
             mNotificationLight.setFlashing(color, Light.LIGHT_FLASH_TIMED, onTime, offTime);
         } else {
             mNotificationLight.turnOff();
