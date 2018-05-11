syrkles420/frameworks_base-1

Sound search tile: Extend the use
Because i think there's people using other apps instead of Google, so i'll use the intents from shazam and soundhound to trigger the search sound engine.
Thanks to the tasker dev to make the intents public.

* Add a toast informing the user about how the tile isn't working.

packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTIle.java
@@ -17,12 +17,16 @@
 package com.android.systemui.qs.tiles;
 
 import android.content.Context;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.service.quicksettings.Tile;
+import android.widget.Toast;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.gzosp.GzospUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -30,8 +34,6 @@ import com.android.systemui.qs.tileimpl.QSTileImpl;
 import com.android.systemui.R;
 
 public class SoundSearchTIle extends QSTileImpl<BooleanState> {
-
-    private final String soundSearchApp = "com.google.android.googlequicksearchbox";
 
     public SoundSearchTIle(QSHost host) {
         super(host);
@@ -45,24 +47,25 @@ public class SoundSearchTIle extends QSTileImpl<BooleanState> {
     @Override
     public void handleClick() {
         mHost.collapsePanels();
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.setAction("com.google.android.googlequicksearchbox.MUSIC_SEARCH");
-        mContext.startActivity(intent);
-    }
-
-    @Override
-    public boolean isAvailable() {
-        return isPackageInstalled();
-    }
-
-    private boolean isPackageInstalled(){
-        try {
-            PackageInfo info = mContext.getPackageManager()
-                    .getPackageInfo(soundSearchApp,PackageManager.GET_META_DATA);
-        } catch (PackageManager.NameNotFoundException e) {
-            return false;
+        // Shazam
+        if (GzospUtils.isPackageInstalled(mContext, "com.shazam.android") || GzospUtils.isPackageInstalled(mContext, "com.shazam.encore.android")) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setAction("com.shazam.android.intent.actions.START_TAGGING");
+            mContext.startActivity(intent);
+        // Soundhound
+        } else if (GzospUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier.freemium") || GzospUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier")) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setAction("com.soundhound.android.ID_NOW_EXTERNAL");
+            mContext.startActivity(intent);
+        // Google Search Music
+        } else if (GzospUtils.isPackageInstalled(mContext, "com.google.android.googlequicksearchbox")) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setAction("com.google.android.googlequicksearchbox.MUSIC_SEARCH");
+            mContext.startActivity(intent);
+        } else {
+            Toast.makeText(mContext, mContext.getString(
+                    R.string.quick_settings_sound_search_no_app), Toast.LENGTH_LONG).show();
         }
-        return true;
     }
 
     @Override

DotOS/android_frameworks_base

Change-Id: Id47ef4218b0b274893b2d8c6b443ac27780fdabe

packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTIle.java
@@ -17,12 +17,16 @@
 package com.android.systemui.qs.tiles;
 
 import android.content.Context;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.service.quicksettings.Tile;
+import android.widget.Toast;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.dotos.DOTUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.qs.QSHost;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -31,38 +35,37 @@ import com.android.systemui.R;
 
 public class SoundSearchTIle extends QSTileImpl<BooleanState> {
 
-    private final String soundSearchApp = "com.google.android.googlequicksearchbox";
-
     public SoundSearchTIle(QSHost host) {
         super(host);
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.QUICK_SETTINGS;
+        return MetricsEvent.DOTEXTRAS;
     }
 
     @Override
     public void handleClick() {
         mHost.collapsePanels();
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.setAction("com.google.android.googlequicksearchbox.MUSIC_SEARCH");
-        mContext.startActivity(intent);
-    }
-
-    @Override
-    public boolean isAvailable() {
-        return isPackageInstalled();
-    }
-
-    private boolean isPackageInstalled(){
-        try {
-            PackageInfo info = mContext.getPackageManager()
-                    .getPackageInfo(soundSearchApp,PackageManager.GET_META_DATA);
-        } catch (PackageManager.NameNotFoundException e) {
-            return false;
+        // Shazam
+        if (GzospUtils.isPackageInstalled(mContext, "com.shazam.android") || GzospUtils.isPackageInstalled(mContext, "com.shazam.encore.android")) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setAction("com.shazam.android.intent.actions.START_TAGGING");
+            mContext.startActivity(intent);
+        // Soundhound
+        } else if (GzospUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier.freemium") || GzospUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier")) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setAction("com.soundhound.android.ID_NOW_EXTERNAL");
+            mContext.startActivity(intent);
+        // Google Search Music
+        } else if (GzospUtils.isPackageInstalled(mContext, "com.google.android.googlequicksearchbox")) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setAction("com.google.android.googlequicksearchbox.MUSIC_SEARCH");
+            mContext.startActivity(intent);
+        } else {
+            Toast.makeText(mContext, mContext.getString(
+                    R.string.quick_settings_sound_search_no_app), Toast.LENGTH_LONG).show();
         }
-        return true;
     }
 
     @Override