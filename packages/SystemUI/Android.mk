35cb2a5a03e418d979580c0e05792dbad8ea5543
IacobIonut01 <paulionut2003@gmail.com>
2018-05-15 21:32:14 +0300

    Implement Custom VolumeDialog
    Design Inspired from Android P [DP1 & DP2]
    
    Change-Id: I7f37fdaaa6628801650584c0afab1c89318c85b1
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -37,6 +37,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-recyclerview \
     android-support-v7-preference \
     android-support-v7-appcompat \
+	android-support-v7-cardview \
     android-support-v7-mediarouter \
     android-support-v7-palette \
     android-support-v14-preference \
