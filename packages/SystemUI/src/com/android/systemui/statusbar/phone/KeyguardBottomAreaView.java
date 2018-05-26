omnirom/android_frameworks_base_old

[1/2] base: hide indicator text
Hide charging information and unlock hints
Kang from: 90f446b30b0deaf32680c947ef960cd702acf0b1

Change-Id: I1cc49cfe7fa9bd41b83e3a96fccbe94421f4e497

packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -48,6 +48,7 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.service.media.CameraPrewarmService;
 import android.telecom.TelecomManager;
 import android.text.TextUtils;
@@ -171,6 +172,9 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private boolean mDozing;
     private boolean mIsPowerCameraGesture;
     private String mPreviousSource;
+
+    // omni additions
+    private boolean mShowIndicator = true;
 
     public KeyguardBottomAreaView(Context context) {
         this(context, null);
@@ -836,9 +840,12 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     }
 
     public void onKeyguardShowingChanged() {
+        mShowIndicator = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.LOCK_HIDE_INDICATOR_DISPLAY, 0, UserHandle.USER_CURRENT) == 0;
         updateLeftAffordance();
         updateRightAffordance();
         inflateCameraPreview();
+        mIndicationController.setVisibleOverwrite(mShowIndicator);
     }
 
     private void setRightButton(IntentButton button) {

DotOS/android_frameworks_base

Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -46,6 +46,7 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.service.media.CameraPrewarmService;
 import android.telecom.TelecomManager;
 import android.text.TextUtils;
@@ -166,6 +167,9 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     private String mLeftButtonStr;
     private LockscreenGestureLogger mLockscreenGestureLogger = new LockscreenGestureLogger();
     private boolean mDozing;
+
+    // omni additions
+    private boolean mShowIndicator = true;
 
     public KeyguardBottomAreaView(Context context) {
         this(context, null);
@@ -800,8 +804,11 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     }
 
     public void onKeyguardShowingChanged() {
+        mShowIndicator = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.HIDE_LOCKSCREEN_INDICATOR_DISPLAY, 0, UserHandle.USER_CURRENT) == 0;
         updateLeftAffordance();
         inflateCameraPreview();
+        mIndicationController.setVisibleOverwrite(mShowIndicator);
     }
 
     private void setRightButton(IntentButton button) {