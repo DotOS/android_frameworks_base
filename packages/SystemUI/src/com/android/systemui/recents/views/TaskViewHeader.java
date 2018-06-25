51cedc1df5fcf95ed8bd8a36548cb4b32dd1be0e
Alex <alex@dirtyunicorns.com>
2018-06-25 10:28:35 -0500

    Hide freeform task header icon
    So the question is, why hide this icon?
    
    The answer is simple. This 'mod' is not supported by Google. Is not even a
    mod that's on by default. You literally have to use ADB to make it appear
    and even then is just asking for problems because not all apps are optimized
    to support it (which is the reason even Google doesn't have it on by default).
    
    To learn more about how to enable it via adb, check the link below
    https://medium.com/sentio-superbook/how-to-enable-multi-window-in-android-o-678cced03db2
    
    Change-Id: I6bcc517db2ee9f2f0531114f36ea7bded2a18d56

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
@@ -377,7 +377,7 @@ public class TaskViewHeader extends FrameLayout
 
         mTitleView.setVisibility(showTitle ? View.VISIBLE : View.INVISIBLE);
         if (mMoveTaskButton != null) {
-            mMoveTaskButton.setVisibility(showMoveIcon ? View.VISIBLE : View.INVISIBLE);
+            mMoveTaskButton.setVisibility(GONE);
             mMoveTaskButton.setTranslationX(rightInset);
         }
         mDismissButton.setVisibility(showDismissIcon ? View.VISIBLE : View.INVISIBLE);
@@ -597,16 +597,17 @@ public class TaskViewHeader extends FrameLayout
             mLockTaskButton.setAlpha(1f);
         }
         if (mMoveTaskButton != null) {
-            if (mMoveTaskButton.getVisibility() == VISIBLE) {
-                mMoveTaskButton.setVisibility(View.VISIBLE);
-                mMoveTaskButton.setClickable(true);
+            if (mMoveTaskButton.getVisibility() == GONE) {
+                mMoveTaskButton.setVisibility(View.GONE);
+                mMoveTaskButton.setClickable(false);
                 mMoveTaskButton.animate()
-                        .alpha(1f)
+                        .alpha(0f)
                         .setInterpolator(Interpolators.FAST_OUT_LINEAR_IN)
                         .setDuration(duration)
                         .start();
             } else {
-                mMoveTaskButton.setAlpha(1f);
+                mMoveTaskButton.setAlpha(0f);
+                mMoveTaskButton.setVisibility(View.GONE);
             }
         }
     }
@@ -625,10 +626,10 @@ public class TaskViewHeader extends FrameLayout
         mLockTaskButton.setAlpha(1f);
         mLockTaskButton.setClickable(true);
         if (mMoveTaskButton != null) {
-            mMoveTaskButton.setVisibility(View.VISIBLE);
+            mMoveTaskButton.setVisibility(View.GONE);
             mMoveTaskButton.animate().cancel();
-            mMoveTaskButton.setAlpha(1f);
-            mMoveTaskButton.setClickable(true);
+            mMoveTaskButton.setAlpha(0f);
+            mMoveTaskButton.setClickable(false);
         }
     }
 
@@ -644,7 +645,7 @@ public class TaskViewHeader extends FrameLayout
         mLockTaskButton.setAlpha(0f);
         mLockTaskButton.setClickable(false);
         if (mMoveTaskButton != null) {
-            mMoveTaskButton.setVisibility(View.INVISIBLE);
+            mMoveTaskButton.setVisibility(View.GONE);
             mMoveTaskButton.setAlpha(0f);
             mMoveTaskButton.setClickable(false);
         }
@@ -673,9 +674,9 @@ public class TaskViewHeader extends FrameLayout
                         Constants.Metrics.DismissSourceHeaderButton);
             }
         } else if (v == mMoveTaskButton) {
-            TaskView tv = Utilities.findParent(this, TaskView.class);
-            EventBus.getDefault().send(new LaunchTaskEvent(tv, mTask, null,
-                    mMoveTaskTargetStackId, false));
+            //TaskView tv = Utilities.findParent(this, TaskView.class);
+            //EventBus.getDefault().send(new LaunchTaskEvent(tv, mTask, null,
+            //        mMoveTaskTargetStackId, false));
         } else if (v == mAppInfoView) {
             EventBus.getDefault().send(new ShowApplicationInfoEvent(mTask));
         } else if (v == mAppIconView) {
