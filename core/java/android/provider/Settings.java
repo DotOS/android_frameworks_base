5789e2c11e2788486969e171f2e442fa79181d1e
calebcabob <calphonic@gmail.com>
2018-10-16 20:45:22 -0500

    Introduce QS tile style picker [1/3]
    This gives users the chance to change the look of their QS tiles.
    Being based on the same methods used by Omni's system theming and
    DU's Fruity Pebbles, it seems to work quite nicely with them.
    
    Originally implimented by calebcabob
    
    - Thanks Mazda-- for the code simplification
    - Thanks eldainosor for improving the logic
    
    Change-Id: I32ac34a5a33e7daead4769d25e89657eb7ff09e6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4667,6 +4667,13 @@ public final class Settings {
                BOOLEAN_VALIDATOR;
 
         /**
+         * Select from various styles to use on the QS tiles
+         *
+         * @hide
+         */
+        public static final String QS_TILE_STYLE = "qs_tile_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
