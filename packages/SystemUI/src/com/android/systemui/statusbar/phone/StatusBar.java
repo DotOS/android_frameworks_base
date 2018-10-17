09444fef0ac48cde2acb7df19d5d1593e2defcf6
calebcabob <calphonic@gmail.com>
2018-10-16 20:45:22 -0500

    Introduce QS tile style picker [1/3]
    This gives users the chance to change the look of their QS tiles.
    Being based on the same methods used by Omni's system theming and
    DU's Fruity Pebbles, it seems to work quite nicely with them.
    
    Originally implimented by calebcabob
    
    - Thanks Mazda-- for the code simplification
    - Thanks eldainosor for improving the logic
    
    Change-Id: I32ac34a5a33e7daead4769d25e89657eb7ff09e6

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4117,6 +4117,18 @@ public class StatusBar extends SystemUI implements DemoMode,
         ThemeAccentUtils.unloadAccents(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
+    // Switches qs tile style from stock to custom
+    public void updateTileStyle() {
+        int qsTileStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_TILE_STYLE, 0, mLockscreenUserManager.getCurrentUserId());
+       ThemeAccentUtils.updateTileStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), qsTileStyle);
+   }
+
+   // Unload all qs tile styles back to stock
+   public void stockTileStyle() {
+       ThemeAccentUtils.stockTileStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+   }
+
     private void updateDozingState() {
         Trace.traceCounter(Trace.TRACE_TAG_APP, "dozing", mDozing ? 1 : 0);
         Trace.beginSection("StatusBar#updateDozingState");
@@ -5324,6 +5336,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.FORCE_AMBIENT_FOR_MEDIA),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                        Settings.System.QS_TILE_STYLE),
+                        false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -5347,6 +5362,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             unloadAccents();
             updateAccents();
             setForceAmbient();
+            } else if (uri.equals(Settings.System.getUriFor(
+                Settings.System.QS_TILE_STYLE))) {
+                stockTileStyle();
+                updateTileStyle();
             }
         }
 
