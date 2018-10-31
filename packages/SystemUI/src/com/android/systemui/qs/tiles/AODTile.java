f267745308fe2fd2150bd88c5828c376d5651e50
SuperDroidBond <superdroidbond@yahoo.com>
2018-10-31 10:31:38 +0530

    AODTile: Fix long click intent
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Id691614c88953dc70aca111630f3b68f82471856

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AODTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AODTile.java
@@ -62,7 +62,8 @@ public class AODTile extends QSTileImpl<BooleanState> {
 
     @Override
     public Intent getLongClickIntent() {
-        return null;
+        return new Intent().setComponent(new ComponentName(
+            "com.custom.ambient.display", "com.custom.ambient.display.DozeService"));
     }
 
     @Override
