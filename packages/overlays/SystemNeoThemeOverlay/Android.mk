9bb37fff18ce47bb68340bb3ec57b57b47edfa57
Mohan Cm <mohanmanjappa@gmail.com>
2019-01-22 23:03:13 +0530

    Neo theme: set SDK version
    Change-Id: I90bd93161e499ebdf0b6a2cf735f42b4828a004b

--- packages/overlays/SystemNeoThemeOverlay/Android.mk
+++ packages/overlays/SystemNeoThemeOverlay/Android.mk
@@ -7,6 +7,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_SDK_VERSION := current
 
 LOCAL_PACKAGE_NAME := SystemNeoThemeOverlay
 
