0169dd1f18ba6f550dafab86ca837fe4550e21d0
DennySPB <dennyspb@gmail.com>
2019-02-27 09:43:36 +0300

    SystemUI: fix statusbar clock overlapping after reboot
    Thx @neobuddy89 for suggestion
    
    Change-Id: I4a000b4afa4eed6ffb3e0f19fe7e43dbe039983f
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mBattery, mClock, mCenterClock, mRightClock, mLogoIcon;
+    private View mLeftSide, mStatusIcons, mBattery, mCenterClock, mLogoIcon;
     private Animator mCurrentAnimation;
     private View mBatteryBar;
 
@@ -47,9 +47,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mLeftSide = mView.findViewById(R.id.status_bar_left_side);
         mStatusIcons = mView.findViewById(R.id.statusIcons);
         mBattery = mView.findViewById(R.id.battery);
-        mClock = mView.findViewById(R.id.clock);
         mCenterClock = mView.findViewById(R.id.center_clock);
-        mRightClock = mView.findViewById(R.id.right_clock);
         mLogoIcon = mView.findViewById(R.id.status_bar_logo);
         mBatteryBar = mView.findViewById(R.id.battery_bar);
         applyModeBackground(-1, getMode(), false /*animate*/);
@@ -97,9 +95,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mBatteryBar, newAlphaBC),
                     animateTransitionTo(mBattery, newAlphaBC),
-                    animateTransitionTo(mClock, newAlphaBC),
                     animateTransitionTo(mCenterClock, newAlphaBC),
-                    animateTransitionTo(mRightClock, newAlphaBC),
                     animateTransitionTo(mLogoIcon, newAlpha)
                     );
             if (isLightsOut(mode)) {
@@ -112,9 +108,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mStatusIcons.setAlpha(newAlpha);
             mBatteryBar.setAlpha(newAlphaBC);
             mBattery.setAlpha(newAlphaBC);
-            mClock.setAlpha(newAlphaBC);
             mCenterClock.setAlpha(newAlphaBC);
-            mRightClock.setAlpha(newAlphaBC);
             mLogoIcon.setAlpha(newAlpha);
         }
     }
