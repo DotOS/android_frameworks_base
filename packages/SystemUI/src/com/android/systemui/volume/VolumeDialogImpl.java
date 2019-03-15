2998222668cb901296396062137f88187b2f5543
rajadeja <raj.jdja@gmail.com>
2019-03-15 13:23:16 +0000

    Add notification slider if ring & notification is unlinked
    Change-Id: I6fdff8a582295d8462588bcc05e17b2e093e9b05

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -453,8 +453,9 @@ public class VolumeDialogImpl implements VolumeDialog {
     private void cleanExpandRows() {
         for(int i = mRows.size() - 1; i >= 0; i--) {
             final VolumeRow row = mRows.get(i);
-            if ((row.stream == AudioManager.STREAM_RING ||
-                    row.stream == AudioManager.STREAM_ALARM) && row.stream != mActiveStream) {
+            if ((row.stream == AudioManager.STREAM_RING
+                    || row.stream == AudioManager.STREAM_NOTIFICATION
+                    || row.stream == AudioManager.STREAM_ALARM) && row.stream != mActiveStream) {
                 Util.setVisOrGone(row.view, /* vis */ false);
             }
         }
@@ -476,6 +477,7 @@ public class VolumeDialogImpl implements VolumeDialog {
         // to our "hardcoded" extra elements.
         int watchAllyStream;
         if (mActiveStream != AudioManager.STREAM_RING
+                || mActiveStream != AudioManager.STREAM_NOTIFICATION
                 || mActiveStream != AudioManager.STREAM_ALARM) {
             watchAllyStream = mActiveStream;
         } else {
@@ -491,6 +493,12 @@ public class VolumeDialogImpl implements VolumeDialog {
                             /* isActive */ row.stream == mActiveStream);
                 }
                 row = findRow(AudioManager.STREAM_ALARM);
+                if (row != null) {
+                    Util.setVisOrGone(row.view, /* vis */ true);
+                    updateVolumeRowTintH(row,
+                            /* isActive */ row.stream == mActiveStream);
+                }
+                row = findRow(AudioManager.STREAM_NOTIFICATION);
                 if (row != null) {
                     Util.setVisOrGone(row.view, /* vis */ true);
                     updateVolumeRowTintH(row,
