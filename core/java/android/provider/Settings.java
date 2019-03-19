c2202c48792821a76fb1e102582d2f27a0e25c5b
ElDainosor <eldainosor@gmail.com>
2019-03-19 01:41:09 -0300

    Add Lockscreen album art filter [1/2]
    @neobuddy89: Improvised and adapted for Android 10 and Android 11/12.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ic557c3eeb3f8bc35a6c9784657ec1e90f749c88d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6146,6 +6146,12 @@ public final class Settings {
         public static final String RETICKER_COLORED = "reticker_colored";
 
         /**
+         * Switch between filters for lockscreen album art
+         * @hide
+         */
+        public static final String LOCKSCREEN_ALBUMART_FILTER = "lockscreen_albumart_filter";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
