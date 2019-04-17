5eddbbdbb93b2b470f9ad2a2def37034b8a2c372
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-04-17 23:26:17 +0000

    base: CustomTextClock: Refactor a few strings
    6cda00d7393fba82546fd5bcdc28ab93546cc133 introduced the guard on system language via intent, but didn't took into account a couple of factors:
    - since we're reassigning vars on language change we need them to not be final
    - there were a couple of strings obtained via R.string method, this leaded to non updated strings
    - the top text was "hardcoded" as a string, so, it was escaping language guard (same applies for highnoon strings)
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I31e8957b95b30c0ef321f8c76f5d134a3abc9733

--- packages/SystemUI/src/com/android/keyguard/clocks/CustomTextClock.java
+++ packages/SystemUI/src/com/android/keyguard/clocks/CustomTextClock.java
@@ -62,6 +62,9 @@ public class CustomTextClock extends TextView {
     private String[] UnitsStringH = getResources().getStringArray(R.array.UnitsStringH);
     private String[] langExceptions = getResources().getStringArray(R.array.langExceptions);
     private String curLang = Locale.getDefault().getLanguage();
+    private String topText = getResources().getString(R.string.custom_text_clock_top_text_default);
+    private String highNoonFirstRow = getResources().getString(R.string.high_noon_first_row);
+    private String highNoonSecondRow = getResources().getString(R.string.high_noon_second_row);
 
     private Time mCalendar;
 
@@ -139,6 +142,7 @@ public class CustomTextClock extends TextView {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (handType == 2) {
+            setText(topText);
             Bitmap mBitmap;
             //Get wallpaper as bitmap
             WallpaperManager manager = WallpaperManager.getInstance(mContext);
@@ -191,7 +195,7 @@ public class CustomTextClock extends TextView {
         switch(handType){
             case 0:
                 if (hour == 12 && minute == 0) {
-                setText(R.string.high_noon_first_row);
+                setText(highNoonFirstRow);
                 } else {
                 setText(getIntStringHour(hour));
                 }
@@ -222,6 +226,10 @@ public class CustomTextClock extends TextView {
                 UnitsString = getResources().getStringArray(R.array.UnitsString);
                 TensStringH = getResources().getStringArray(R.array.TensStringH);
                 UnitsStringH = getResources().getStringArray(R.array.UnitsStringH);
+                curLang = Locale.getDefault().getLanguage();
+                topText = getResources().getString(R.string.custom_text_clock_top_text_default);
+                highNoonFirstRow = getResources().getString(R.string.high_noon_first_row);
+                highNoonSecondRow = getResources().getString(R.string.high_noon_second_row);
             }
             onTimeChanged();
 
