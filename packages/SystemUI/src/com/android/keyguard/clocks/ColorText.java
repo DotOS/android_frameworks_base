7d499dbb1552ff61b030828e1519123693ed7c94
ovenoboyo <ovenoboyo@gmail.com>
2019-04-18 18:43:03 +0530

    CustomTextClock: Improvements and translations import from Desendent & Pearl
    Change-Id: I14cf2ef11ef287f880d281a152c158efeb459c5d
    Co-authored-by: Dil3mm4 <dil3mm4.dev@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clocks/ColorText.java
+++ packages/SystemUI/src/com/android/keyguard/clocks/ColorText.java
@@ -18,40 +18,38 @@ import java.lang.IllegalStateException;
 public class ColorText {
 
     public static int getWallColor(Context mContext) {
-            Bitmap mBitmap;
-            //Get wallpaper as bitmap
-            WallpaperManager manager = WallpaperManager.getInstance(mContext);
-            ParcelFileDescriptor pfd = manager.getWallpaperFile(WallpaperManager.FLAG_LOCK);
+        Bitmap mBitmap;
+        //Get wallpaper as bitmap
+        WallpaperManager manager = WallpaperManager.getInstance(mContext);
+        ParcelFileDescriptor pfd = manager.getWallpaperFile(WallpaperManager.FLAG_LOCK);
 
-            //Sometimes lock wallpaper maybe null as getWallpaperFile doesnt return builtin wallpaper
-            if (pfd == null)
-                pfd = manager.getWallpaperFile(WallpaperManager.FLAG_SYSTEM);
+        //Sometimes lock wallpaper maybe null as getWallpaperFile doesnt return builtin wallpaper
+        if (pfd == null)
+            pfd = manager.getWallpaperFile(WallpaperManager.FLAG_SYSTEM);
 
-            try {
-                if (pfd != null) {
-                    mBitmap = BitmapFactory.decodeFileDescriptor(pfd.getFileDescriptor());
-                } else {
-                    //Incase both cases return null wallpaper, generate a yellow bitmap
-                    mBitmap = drawEmpty();
-                }
-                Palette palette = Palette.generate(mBitmap);
-
-                //For monochrome and single color bitmaps, the value returned is 0
-                if (Color.valueOf(palette.getLightVibrantColor(0x000000)).toArgb() == 0) {
-                    //So get bodycolor on dominant color instead as a hacky workaround
-                    return Color.valueOf(palette.getDominantSwatch().getBodyTextColor()).toArgb();
-
-                //On Black Wallpapers set color to White
-                } else if(String.format("#%06X", (0xFFFFFF & (palette.getLightVibrantColor(0x000000)))) == "#000000") {
-                    return Color.WHITE;
-                } else {
-                    return Color.valueOf(palette.getLightVibrantColor(0xff000000)).toArgb();
-                }
-
-              //Just a fallback, although I doubt this case will ever come
-            } catch (NullPointerException e) {
-                return Color.WHITE;
+        try {
+            if (pfd != null) {
+                mBitmap = BitmapFactory.decodeFileDescriptor(pfd.getFileDescriptor());
+            } else {
+                //Incase both cases return null wallpaper, generate a yellow bitmap
+                mBitmap = drawEmpty();
             }
+            Palette palette = Palette.generate(mBitmap);
+
+            //For monochrome and single color bitmaps, the value returned is 0
+            if (Color.valueOf(palette.getLightVibrantColor(0x000000)).toArgb() == 0) {
+                //So get bodycolor on dominant color instead as a hacky workaround
+                return Color.valueOf(palette.getDominantSwatch().getBodyTextColor()).toArgb();
+
+            //On Black Wallpapers set color to White
+            } else if(String.format("#%06X", (0xFFFFFF & (palette.getLightVibrantColor(0x000000)))) == "#000000") {
+                return Color.WHITE;
+            } else {
+                return Color.valueOf(palette.getLightVibrantColor(0xff000000)).toArgb();
+            }
+        } catch (NullPointerException e) {
+            return Color.WHITE;
+        }
     }
 
     private static Bitmap drawEmpty() {
