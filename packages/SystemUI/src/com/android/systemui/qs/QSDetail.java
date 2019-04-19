1932dcf4755bb52f5d5dbf7fb8b3117f116cd0ef
IacobIonut01 <paulionut2003@gmail.com>
2019-04-19 23:41:18 +0300

    Redesign QSPanel
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: I20dc31002215d826bb51ebfe8ee97305fa60a45b

--- packages/SystemUI/src/com/android/systemui/qs/QSDetail.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSDetail.java
@@ -23,6 +23,7 @@ import android.annotation.Nullable;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.drawable.Animatable;
 import android.util.AttributeSet;
 import android.util.SparseArray;
@@ -31,6 +32,7 @@ import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -85,7 +87,13 @@ public class QSDetail extends LinearLayout {
         super.onConfigurationChanged(newConfig);
         FontSizeUtils.updateFontSize(mDetailDoneButton, R.dimen.qs_detail_button_text_size);
         FontSizeUtils.updateFontSize(mDetailSettingsButton, R.dimen.qs_detail_button_text_size);
-
+        /*Resources resources = getContext().getResources();
+        int relativeHeight = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE ?
+        resources.getDimensionPixelSize(com.android.internal.R.dimen.quick_qs_offset_height)*2 :
+        resources.getDimensionPixelSize(com.android.internal.R.dimen.quick_qs_offset_height);
+        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) getLayoutParams();
+        lp.topMargin = relativeHeight;
+        setLayoutParams(lp);*/
         for (int i = 0; i < mDetailViews.size(); i++) {
             mDetailViews.valueAt(i).dispatchConfigurationChanged(newConfig);
         }
@@ -112,7 +120,7 @@ public class QSDetail extends LinearLayout {
 
         updateDetailText();
 
-        mClipper = new QSDetailClipper(findViewById(R.id.detail_container));
+        mClipper = new QSDetailClipper(findViewById(R.id.qs_detail_base_layout));
 
         final OnClickListener doneListener = new OnClickListener() {
             @Override
