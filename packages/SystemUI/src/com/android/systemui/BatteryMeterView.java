205dd8e743691344870c3e0b8c15ea960baea3aa
paphonb <paphonb@gmail.com>
2019-05-09 20:09:14 +0000

    Fix weird artifact in battery icon
    Change-Id: I3ee6a8f83109395479322f254f0b32f60665f930

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -158,6 +158,7 @@ public class BatteryMeterView extends LinearLayout implements
 
         setClipChildren(false);
         setClipToPadding(false);
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 
     public void setForceShowPercent(boolean show) {
