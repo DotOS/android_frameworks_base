946622c8f5bcbd59d6d56bb7480e8f7df529179e
Mohan Cm <mohanmanjappa@gmail.com>
2019-05-31 08:07:23 +0000

    Clean up ambient display and aod commits
    Revert "base: add option to show AOD on charge only [1/2]"
    This reverts commit 1fa13a6583c2a6835aa18b75c28e0b135c6f1289.
    
    Revert "Add Always On Display QS Tile"
    This reverts commit 1886e6b17a72578b87343c94b1d2affe79f27295.
    
    Revert "AODTile: Fix long click intent"
    This reverts commit d8235233360c61d73fb60e194065d80c30af62aa.
    
    Revert "SystemUI: Add AmbientDisplay tile"
    This revert commit 43fa2fc080f6a14026300e1c1e3e4cb7676e9f82.
    
    Change-Id: Ie137fcd60a05ed55963cf6c7cba1bda9e54811e5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/DozeParameters.java
@@ -31,10 +31,11 @@ import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.doze.AlwaysOnDisplayPolicy;
 import com.android.systemui.doze.DozeScreenState;
+import com.android.systemui.tuner.TunerService;
 
 import java.io.PrintWriter;
 
-public class DozeParameters {
+public class DozeParameters implements TunerService.Tunable {
     private static final int MAX_DURATION = 60 * 1000;
     public static final String DOZE_SENSORS_WAKE_UP_FULLY = "doze_sensors_wake_up_fully";
     public static final boolean FORCE_NO_BLANKING =
@@ -49,6 +50,7 @@ public class DozeParameters {
 
     private final AlwaysOnDisplayPolicy mAlwaysOnPolicy;
 
+    private boolean mDozeAlwaysOn;
     private boolean mControlScreenOffAnimation;
 
     public static DozeParameters getInstance(Context context) {
@@ -67,6 +69,9 @@ public class DozeParameters {
         mControlScreenOffAnimation = !getDisplayNeedsBlanking();
         mPowerManager = mContext.getSystemService(PowerManager.class);
         mPowerManager.setDozeAfterScreenOff(!mControlScreenOffAnimation);
+
+        Dependency.get(TunerService.class).addTunable(this, Settings.Secure.DOZE_ALWAYS_ON,
+                Settings.Secure.ACCESSIBILITY_DISPLAY_INVERSION_ENABLED);
     }
 
     public void dump(PrintWriter pw) {
@@ -168,7 +173,7 @@ public class DozeParameters {
      * @return {@code true} if enabled and available.
      */
     public boolean getAlwaysOn() {
-        return mAmbientDisplayConfiguration.alwaysOnEnabled(UserHandle.USER_CURRENT);
+        return mDozeAlwaysOn;
     }
 
     /**
@@ -235,6 +240,11 @@ public class DozeParameters {
 
     public boolean doubleTapReportsTouchCoordinates() {
         return mContext.getResources().getBoolean(R.bool.doze_double_tap_reports_touch_coordinates);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        mDozeAlwaysOn = mAmbientDisplayConfiguration.alwaysOnEnabled(UserHandle.USER_CURRENT);
     }
 
     public AlwaysOnDisplayPolicy getPolicy() {
