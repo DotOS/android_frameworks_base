d8182171ee627cad01a4080c894f3fb146797f50
ElDainosor <eldainosor@gmail.com>
2019-08-25 17:52:29 +0530

    ImageUtils: Switch blur lib to SimpleImageBlur
    Let's use their blurRenderScript, and fix our glitchy images on other media players (Example: Phonograph, Play Music, Black player)
    Source:
    	https://github.com/Cutta/Simple-Image-Blur/blob/master/app/src/main/java/com/example/cunoraz/ViewPagerAdapter.java#L73
    
    Change-Id: I069da8ae6d32638af783a0027525ba8087730dbb

--- core/java/com/android/internal/util/dotos/ImageHelper.java
+++ core/java/com/android/internal/util/dotos/ImageHelper.java
@@ -153,23 +153,43 @@ public class ImageHelper {
     }
 
     public static Bitmap getBlurredImage(Context context, Bitmap image, float radius) {
-        float BITMAP_SCALE = 0.4f;
+        try {
+            image = RGB565toARGB888(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        int width = Math.round(image.getWidth() * BITMAP_SCALE);       
-        int height = Math.round(image.getHeight() * BITMAP_SCALE);
+        Bitmap bitmap = Bitmap.createBitmap(
+                image.getWidth(), image.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        RenderScript renderScript = RenderScript.create(context);
+        Allocation blurInput = Allocation.createFromBitmap(renderScript, image);
+        Allocation blurOutput = Allocation.createFromBitmap(renderScript, bitmap);
 
-        Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
-        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+        ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(renderScript,
+                Element.U8_4(renderScript));
+        blur.setInput(blurInput);
+        blur.setRadius(radius); // radius must be 0 < r <= 25
+        blur.forEach(blurOutput);
+        blurOutput.copyTo(bitmap);
+        renderScript.destroy();
 
-        RenderScript rs = RenderScript.create(context);
-        ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
-        Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
-        Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);  
-        theIntrinsic.setRadius(radius);
-        theIntrinsic.setInput(tmpIn);
-        theIntrinsic.forEach(tmpOut);
-        tmpOut.copyTo(outputBitmap);
-        return outputBitmap;
+        return bitmap;
    }
+
+    private static Bitmap RGB565toARGB888(Bitmap img) throws Exception {
+        int numPixels = img.getWidth() * img.getHeight();
+        int[] pixels = new int[numPixels];
+
+        //Get JPEG pixels.  Each int is the color values for one pixel.
+        img.getPixels(pixels, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+
+        //Create a Bitmap of the appropriate format.
+        Bitmap result = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
+
+        //Set RGB pixels.
+        result.setPixels(pixels, 0, result.getWidth(), 0, 0, result.getWidth(), result.getHeight());
+        return result;
+    }
 
 }
