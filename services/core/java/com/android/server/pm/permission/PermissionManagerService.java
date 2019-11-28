521213db81329f82b55dacbee5678a99b50439f9
Surge Raval <Surge1223@gmail.com>
2019-11-28 17:40:46 +0100

    Grant Wellbeing the SUSPEND_APPS perm if already declared
    @saikiran2001: adapt to android 12
    
    Change-Id: I4ca48f93888cc187f4918699fe896a2176b63953
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- services/core/java/com/android/server/pm/permission/PermissionManagerService.java
+++ services/core/java/com/android/server/pm/permission/PermissionManagerService.java
@@ -1502,6 +1502,7 @@ public class PermissionManagerService extends IPermissionManager.Stub {
 
             isRuntimePermission = bp.isRuntime();
             permissionHasGids = bp.hasGids();
+            if (pkg.getPackageName() != "com.google.android.apps.wellbeing") {
             if (isRuntimePermission || bp.isDevelopment()) {
                 // Good.
             } else if (bp.isRole()) {
@@ -1512,6 +1513,7 @@ public class PermissionManagerService extends IPermissionManager.Stub {
                 throw new SecurityException("Permission " + permName + " requested by "
                         + pkg.getPackageName() + " is not a changeable permission type");
             }
+        }
 
             final UidPermissionState uidState = getUidStateLocked(pkg, userId);
             if (uidState == null) {
