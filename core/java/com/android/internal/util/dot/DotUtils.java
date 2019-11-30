d8e7def8b84e21c33dcdbbac9ee4c860a048cc23
Anushek Prasal <anushekprasal@gmail.com>
2019-11-30 16:08:56 +0530

    DotUtils: Update notch check logic
    Some devices with notch have set config_fillMainBuiltInDisplayCutout to false.
    This results in Utils returning true for hasNotch.
    Instead, let's check if the bounding path of the cutout region is defined.
    Also return false if NoCutoutOverlay is active.
    
    Change-Id: Ib134312d1c49b8ad8c086a1271cc29f6df020f3c

--- core/java/com/android/internal/util/dot/DotUtils.java
+++ core/java/com/android/internal/util/dot/DotUtils.java
@@ -40,6 +40,7 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.IWindowManager;
 import android.view.InputDevice;
@@ -293,21 +294,10 @@ public class DotUtils {
 
     // Check if device has a notch
     public static boolean hasNotch(Context context) {
-        int result = 0;
-        int resid;
-        int resourceId = context.getResources().getIdentifier(
-                "status_bar_height", "dimen", "android");
-        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
-                "bool", "android");
-        if (resid > 0) {
-            return context.getResources().getBoolean(resid);
-        }
-        if (resourceId > 0) {
-            result = context.getResources().getDimensionPixelSize(resourceId);
-        }
-        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
-        float px = 24 * (metrics.densityDpi / 160f);
-        return result > Math.round(px);
+        String displayCutout = context.getResources().getString(R.string.config_mainBuiltInDisplayCutout);
+        boolean maskDisplayCutout = context.getResources().getBoolean(R.bool.config_maskMainBuiltInDisplayCutout);
+        boolean displayCutoutExists = (!TextUtils.isEmpty(displayCutout) && !maskDisplayCutout);
+        return displayCutoutExists;
     }
 
     /**
