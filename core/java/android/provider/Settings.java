edf02a33b6ce63dc4bb89510696ad458c893a03d
Ido Ben-Hur <idoybh2@gmail.com>
2020-02-03 15:48:29 +0200

    SystemUI: Pulse edge light for all doze triggers [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6246,6 +6246,11 @@ public final class Settings {
         public static final String NOTIFICATION_PULSE_DURATION = "ambient_notification_light_duration";
 
         /**
+         * @hide
+         */
+        public static final String AMBIENT_LIGHT_PULSE_FOR_ALL = "ambient_light_pulse_for_all";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
