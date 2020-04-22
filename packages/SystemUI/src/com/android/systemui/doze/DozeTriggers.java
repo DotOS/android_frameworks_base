4e8804540c2cecdfcf2c7197f9b8ab169f7a49ea
PMS22 <pratham220399@gmail.com>
2020-04-22 13:40:22 +0530

    AmbientDisplay: Unregister proximity when screen off fod is active
    - Saves a hell lot of battery
    
    Signed-off-by: PMS22 <prathams99@rediff.com>
    Change-Id: I1988d3c3b3ec6a0f338d1ecbcbd27c8ae818fa29
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -378,6 +378,10 @@ public class DozeTriggers implements DozeMachine.Part {
 
     @Override
     public void transitionTo(DozeMachine.State oldState, DozeMachine.State newState) {
+
+        boolean screenOffFod = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FOD_GESTURE, 0) != 0;
+
         switch (newState) {
             case INITIALIZED:
                 mBroadcastReceiver.register(mBroadcastDispatcher);
@@ -393,6 +397,10 @@ public class DozeTriggers implements DozeMachine.Part {
                 mWantTouchScreenSensors = true;
                 if (newState == DozeMachine.State.DOZE_AOD && !sWakeDisplaySensorState) {
                     onWakeScreen(false, newState);
+                }
+                if (screenOffFod) {
+                    mDozeSensors.setProxListening(false);
+                    mDozeSensors.setListening(false);
                 }
                 break;
             case DOZE_AOD_PAUSED:
