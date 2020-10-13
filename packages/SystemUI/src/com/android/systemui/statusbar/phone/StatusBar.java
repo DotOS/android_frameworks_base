771e5c8a92ce3e908a634bdb7c8e34aec568b10e
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-10-13 09:29:19 +0000

    StatusBar: dismiss qs when screen's going off if showing
    test: expand to full QS state, lock device, unlock device, ensure QS isn't expanded anymore
    Change-Id: Ib3ba1eb4c17a8cf396efcc2be378513869b0f904

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4179,6 +4179,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         public void onScreenTurnedOff() {
             mFalsingManager.onScreenOff();
             mScrimController.onScreenTurnedOff();
+            if (mNotificationPanelViewController.isQsExpanded()) {
+                mNotificationPanelViewController.closeQs();
+            }
             updateIsKeyguard();
         }
     };
