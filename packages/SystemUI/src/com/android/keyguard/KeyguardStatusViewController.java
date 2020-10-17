088bfaa61e970f3c7f742a373b7e49cbf9ad0492
Marko Man <darkobas@gmail.com>
2020-10-17 21:42:05 +0200

    SystemUI: Add Ambient Lights always on hide aod content option
    idoybh remarks: Omni likes to add the setting in the wrong commit...
    I'm lazy so let's keep it that way
    
    Also adapted to Sv2

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusViewController.java
@@ -18,6 +18,7 @@ package com.android.keyguard;
 
 import android.graphics.Rect;
 import android.util.Slog;
+import android.view.View;
 
 import com.android.keyguard.KeyguardClockSwitch.ClockSize;
 import com.android.systemui.keyguard.KeyguardUnlockAnimationController;
@@ -107,6 +108,14 @@ public class KeyguardStatusViewController extends ViewController<KeyguardStatusV
     public void dozeTimeTick() {
         refreshTime();
         mKeyguardSliceViewController.refresh();
+    }
+
+    /**
+     * Sets the view visibility
+     * @hide
+     */
+    public void setViewVisible(boolean visible) {
+        mView.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
     }
 
     /**
