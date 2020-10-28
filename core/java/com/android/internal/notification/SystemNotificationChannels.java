9df1e65c45d296c1b17f7d86c3c408f450abf62e
maxwen <max.weninger@gmail.com>
2020-10-28 15:40:01 +0100

    Add more notification channels to be blockable
    * VPN
    * USB
    
    Change-Id: I5443d46d5141a73a537057ee0fed3f55cb59661d

--- core/java/com/android/internal/notification/SystemNotificationChannels.java
+++ core/java/com/android/internal/notification/SystemNotificationChannels.java
@@ -139,6 +139,7 @@ public class SystemNotificationChannels {
                 VPN,
                 context.getString(R.string.notification_channel_vpn),
                 NotificationManager.IMPORTANCE_LOW);
+        vpn.setBlockable(true);
         channelsList.add(vpn);
 
         final NotificationChannel deviceAdmin = new NotificationChannel(
