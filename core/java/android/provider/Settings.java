aaa3c990baa9491d1863c459b8ba1dd95dfda1b6
Joey Huab <joey@evolution-x.org>
2020-12-01 06:50:16 +0900

    [SQUASH] SystemUI: FOD pressed colors [1/2]
    Includes the following:
    * https://github.com/Evolution-X/frameworks_base/commit/b70ac04e293c7a2eea2710a1a689630a74e70364
    * https://github.com/Evolution-X/frameworks_base/commit/6ecbadff3550c01682a15331f3ccb0abf614d845

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4816,6 +4816,12 @@ public final class Settings {
          */
         public static final String FOD_ICON = "fod_icon";
 
+        /**
+         * FOD pressed color
+         * @hide
+         */
+        public static final String FOD_COLOR = "fod_color";
+
         /*
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
