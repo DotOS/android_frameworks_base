0cd5ca9ad60f4179e11dee7f5c54abd3d33ccb0f
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-01-02 00:09:44 +0530

    SystemUI: It's time to kill klondike-saur
    Change-Id: I06c64b79727379d8fa42ed6735893c9ee58dd583
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/5190
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPINView.java
@@ -47,7 +47,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     private ViewGroup mRow1;
     private ViewGroup mRow2;
     private ViewGroup mRow3;
-    private View mDivider;
     private int mDisappearYTranslation;
     private View[][] mViews;
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
@@ -99,7 +98,6 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
         mRow1 = findViewById(R.id.row1);
         mRow2 = findViewById(R.id.row2);
         mRow3 = findViewById(R.id.row3);
-        mDivider = findViewById(R.id.divider);
         mViews = new View[][]{
                 new View[]{
                         mRow0, null, null
