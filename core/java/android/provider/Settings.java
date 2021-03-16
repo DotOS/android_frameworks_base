47e157f524b6bf09896cdbedeaa88e3548be2e7d
Ido Ben-Hur <idoybh2@gmail.com>
2021-03-16 13:53:27 +0200

    AutoAODService: Add support for mixed time & sun modes [1/2]
    Also refactor some code
    
    Change-Id: I3a78dec88f532766d00e0d1d276c27dc9d3dc68f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9472,6 +9472,8 @@ public final class Settings {
          * 0 = disabled (default)
          * 1 = from sunset to sunrise
          * 2 = custom time
+         * 3 = from sunset till a time
+         * 4 = from a time till sunrise
          * @hide
          */
         @Readable
@@ -9479,7 +9481,7 @@ public final class Settings {
 
         /**
          * The custom time {@link DOZE_ALWAYS_ON} should be on at
-         * Only relevant when {@link DOZE_ALWAYS_ON_AUTO_MODE} is set to 2
+         * Only relevant when {@link DOZE_ALWAYS_ON_AUTO_MODE} is set to 2 and above
          * 0 = Disabled (default)
          * format: HH:mm,HH:mm (since,till)
          * @hide
