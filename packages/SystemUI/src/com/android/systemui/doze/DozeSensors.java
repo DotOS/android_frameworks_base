a79b7b836c3014ccb44096d27976b5d7f6cd801c
daniml3 <danimoral1001@gmail.com>
2021-05-06 14:15:35 +0530

    fixup! DozeSensors: Only use proximity sensor if supported
    * Previous logic inverted the boolean and broke proximity sensor turning off AOD when is covered
    
    Change-Id: I6d51edbfdb9dc0976bfa773c1e6daafd671019ac

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -81,7 +81,7 @@ public class DozeSensors {
     private long mDebounceFrom;
     private boolean mSettingRegistered;
     private boolean mListening;
-    private boolean mDisableProx;
+    private boolean mProximitySupported;
 
     @VisibleForTesting
     public enum DozeSensorsUiEvent implements UiEventLogger.UiEventEnum {
@@ -114,7 +114,7 @@ public class DozeSensors {
         mResolver = mContext.getContentResolver();
         mCallback = callback;
         mProximitySensor = proximitySensor;
-        mDisableProx = context.getResources().getBoolean(R.bool.doze_proximity_sensor_supported);
+        mProximitySupported = context.getResources().getBoolean(R.bool.doze_proximity_sensor_supported);
 
         boolean alwaysOn = mConfig.alwaysOnEnabled(UserHandle.USER_CURRENT);
         mSensors = new TriggerSensor[] {
@@ -177,7 +177,7 @@ public class DozeSensors {
                         dozeLog),
         };
 
-        if (!mDisableProx) {
+        if (mProximitySupported) {
             setProxListening(false);  // Don't immediately start listening when we register.
             mProximitySensor.register(
                     proximityEvent -> {
@@ -317,7 +317,7 @@ public class DozeSensors {
         for (TriggerSensor s : mSensors) {
             pw.println("  Sensor: " + s.toString());
         }
-        if (!mDisableProx) // Useless
+        if (mProximitySupported) // Useless
             pw.println("  ProxSensor: " + mProximitySensor.toString());
     }
 
@@ -325,7 +325,7 @@ public class DozeSensors {
      * @return true if prox is currently near, false if far or null if unknown.
      */
     public Boolean isProximityCurrentlyNear() {
-        return mDisableProx ? null : mProximitySensor.isNear();
+        return !mProximitySupported ? null : mProximitySensor.isNear();
     }
 
     @VisibleForTesting
