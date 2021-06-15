21e3bee43ef2c7368b1fc80c842874c31107dc76
DarkJoker360 <simoespo159@gmail.com>
2021-06-15 03:47:59 -0700

    Revert "DO NOT MERGE Disable privacy indicators"
    This reverts commit 2628cf48a15acf1f1126d15ba9d1b3c43d934967.
    
    Change-Id: I11859a0d9189f85468904aebbeef3fab459456d6

--- packages/SystemUI/src/com/android/systemui/privacy/PrivacyItemController.kt
+++ packages/SystemUI/src/com/android/systemui/privacy/PrivacyItemController.kt
@@ -106,9 +106,9 @@ class PrivacyItemController @Inject constructor(
         uiExecutor.execute(notifyChanges)
     }
 
-    var allIndicatorsAvailable = false
+    var allIndicatorsAvailable = isAllIndicatorsEnabled()
         private set
-    var micCameraAvailable = false
+    var micCameraAvailable = isMicCameraEnabled()
         private set
 
     private val devicePropertiesChangedListener =
@@ -160,6 +160,10 @@ class PrivacyItemController @Inject constructor(
         }
 
     init {
+        deviceConfigProxy.addOnPropertiesChangedListener(
+                DeviceConfig.NAMESPACE_PRIVACY,
+                uiExecutor,
+                devicePropertiesChangedListener)
         dumpManager.registerDumpable(TAG, this)
     }
 
