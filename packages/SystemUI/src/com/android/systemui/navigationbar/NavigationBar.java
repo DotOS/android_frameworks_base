797b26bed5f227dd74a86c3e1bdf34d9a95d0a17
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-14 22:07:17 +0200

    NavigationBar: Prevent a rare crash
    Only happens after setup. Add an NPE check
    
    Log:
    E AndroidRuntime: Process: com.android.systemui, PID: 2994
    E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void com.android.systemui.navigationbar.NavigationBarView.setAccessibilityButtonState(boolean, boolean)' on a null object reference
    E AndroidRuntime:        at com.android.systemui.navigationbar.NavigationBar.updateAccessibilityServicesState(NavigationBar.java:1241)
    E AndroidRuntime:        at com.android.systemui.navigationbar.NavigationBar$$ExternalSyntheticLambda10.onAccessibilityServicesStateChanged(Unknown Source:2)
    E AndroidRuntime:        at android.view.accessibility.AccessibilityManager$1.lambda$notifyServicesStateChanged$0$AccessibilityManager$1(AccessibilityManager.java:419)
    E AndroidRuntime:        at android.view.accessibility.AccessibilityManager$1$$ExternalSyntheticLambda0.run(Unknown Source:4)
    ...

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBar.java
@@ -1235,7 +1235,8 @@ public class NavigationBar implements View.OnAttachStateChangeListener,
         if (mNavigationBarView != null) {
             boolean clickable = (a11yFlags & SYSUI_STATE_A11Y_BUTTON_CLICKABLE) != 0;
             boolean longClickable = (a11yFlags & SYSUI_STATE_A11Y_BUTTON_LONG_CLICKABLE) != 0;
-            mNavigationBarView.setAccessibilityButtonState(clickable, longClickable);
+            if (mNavigationBarView != null)
+                mNavigationBarView.setAccessibilityButtonState(clickable, longClickable);
         }
         updateSystemUiStateFlags(a11yFlags);
     }
