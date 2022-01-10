20a2b7b63e87cd824c456a0b57815620bf661cd8
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-01-10 11:19:24 +0100

    KeyEvent.java: Use home key as wake up key.
    Change-Id: I102e50d410d651415b7e9910674e66810c33e5bd

--- core/java/android/view/KeyEvent.java
+++ core/java/android/view/KeyEvent.java
@@ -1988,6 +1988,7 @@ public class KeyEvent extends InputEvent implements Parcelable {
         switch (keyCode) {
             case KeyEvent.KEYCODE_CAMERA:
             case KeyEvent.KEYCODE_MENU:
+            case KeyEvent.KEYCODE_HOME:
             case KeyEvent.KEYCODE_PAIRING:
             case KeyEvent.KEYCODE_STEM_1:
             case KeyEvent.KEYCODE_STEM_2:
