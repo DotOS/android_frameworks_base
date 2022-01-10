c2a6cf925df0db2e538640105a15b0fa86635f29
Pranav Vashi <neobuddy89@gmail.com>
2022-01-10 12:53:25 +0530

    ColorUtils: Prevent crash if alpha component is translucent
    * Fixes crash in some instances like Launcher icon edit, Pulse visualizer, etc.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I57fc19d29c96ac76dcbbd6badadca65068c37966

--- core/java/com/android/internal/graphics/ColorUtils.java
+++ core/java/com/android/internal/graphics/ColorUtils.java
@@ -22,6 +22,7 @@ import android.annotation.IntRange;
 import android.annotation.NonNull;
 import android.graphics.Color;
 import android.util.MathUtils;
+import android.util.Log;
 
 import com.android.internal.graphics.cam.Cam;
 
@@ -31,6 +32,8 @@ import com.android.internal.graphics.cam.Cam;
  * A set of color-related utility methods, building upon those available in {@code Color}.
  */
 public final class ColorUtils {
+
+    private static final String TAG = "ColorUtils";
 
     private static final double XYZ_WHITE_REFERENCE_X = 95.047;
     private static final double XYZ_WHITE_REFERENCE_Y = 100;
@@ -93,8 +96,10 @@ public final class ColorUtils {
      */
     public static double calculateContrast(@ColorInt int foreground, @ColorInt int background) {
         if (Color.alpha(background) != 255) {
-            throw new IllegalArgumentException("background can not be translucent: #"
-                    + Integer.toHexString(background));
+            Log.w(TAG, String.format(
+                    "Background should not be translucent: #%s",
+                    Integer.toHexString(background)));
+            background = setAlphaComponent(background, 255);
         }
         if (Color.alpha(foreground) < 255) {
             // If the foreground is translucent, composite the foreground over the background
@@ -146,8 +151,10 @@ public final class ColorUtils {
     public static int calculateMinimumAlpha(@ColorInt int foreground, @ColorInt int background,
             float minContrastRatio) {
         if (Color.alpha(background) != 255) {
-            throw new IllegalArgumentException("background can not be translucent: #"
-                    + Integer.toHexString(background));
+            Log.w(TAG, String.format(
+                    "Background should not be translucent: #%s",
+                    Integer.toHexString(background)));
+            background = setAlphaComponent(background, 255);
         }
 
         ContrastCalculator contrastCalculator = (fg, bg, alpha) -> {
