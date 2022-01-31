faa56a19fc91669fd4fd724be5e500b765e1f52a
spezi77 <spezi7713@gmx.net>
2022-01-31 19:17:34 +0100

    core: Rework the ph0t0s features blacklist
    * Moving the flags to an array feels more like a blacklist :P
    * Converted the flags into fully qualified package names, while at it
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I4b9e925fc0b8c01204564e18b9e9ee4c7d31c123

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -709,18 +709,26 @@ public class ApplicationPackageManager extends PackageManager {
                 }
             };
 
+    private static final String[] featuresBlacklist = {
+            "com.google.android.apps.photos.PIXEL_2019_PRELOAD",
+            "com.google.android.apps.photos.PIXEL_2019_MIDYEAR_PRELOAD",
+            "com.google.android.apps.photos.PIXEL_2018_PRELOAD",
+            "com.google.android.apps.photos.PIXEL_2017_PRELOAD",
+            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2020_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2020_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2019_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2019_MIDYEAR_EXPERIENCE",
+            "com.google.android.feature.PIXEL_2017_EXPERIENCE"
+    };
+
     @Override
     public boolean hasSystemFeature(String name, int version) {
         String packageName = ActivityThread.currentPackageName();
         if (packageName != null &&
                 packageName.contains("com.google.android.apps.photos") &&
-                name.contains("PIXEL_2021_EXPERIENCE") ||
-                name.contains("PIXEL_2021_MIDYEAR_EXPERIENCE") ||
-                name.contains("PIXEL_2020_EXPERIENCE") ||
-                name.contains("PIXEL_2020_MIDYEAR_EXPERIENCE") ||
-                name.contains("PIXEL_2019_EXPERIENCE") ||
-                name.contains("PIXEL_2019_PRELOAD") ||
-                name.contains("PIXEL_2019_MIDYEAR_EXPERIENCE")) {
+                Arrays.asList(featuresBlacklist).contains(name)) {
             return false;
         }
         return mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
