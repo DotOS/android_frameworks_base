fd3356dc96b405b2cabfb323c59096c925cd6161
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-02-02 23:52:39 +0800

    base: support per-app volume [2/3]
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: Iaa97787d3820b4f6c23fda678ec3ffe711061c4f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5738,6 +5738,12 @@ public final class Settings {
         }
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * When to use Wi-Fi calling
          *
          * @see android.telephony.TelephonyManager.WifiCallingChoices
@@ -17248,6 +17254,12 @@ public final class Settings {
         @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
         public static final String ACTION_VOLUME =
                 "android.settings.panel.action.VOLUME";
+
+        /**
+         * @hide
+         */
+        public static final String ACTION_APP_VOLUME =
+                "android.settings.panel.action.APP_VOLUME";
     }
 
     /**
