343aa44fffd6b01f098a91270cf398a284d04e6c
Ivan Martynov <johnivan19999@gmail.com>
2022-02-18 17:27:50 +0900

    PixelPropsUtils: Remove Pubg New State from GamesProps
    This app runs on UnrealEngine4 with own AntiCheat which is frequently crashed with GamesProps
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/com/android/internal/util/dot/PixelPropsUtils.java
+++ core/java/com/android/internal/util/dot/PixelPropsUtils.java
@@ -90,7 +90,6 @@ public class PixelPropsUtils {
             "com.tencent.ig",
             "com.pubg.imobile",
             "com.pubg.krmobile",
-            "com.pubg.newstate",
             "com.vng.pubgmobile",
             "com.rekoo.pubgm",
             "com.tencent.tmgp.pubgmhd",
