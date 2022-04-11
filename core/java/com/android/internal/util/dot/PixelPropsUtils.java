c0e88049a288d4684d28ed70d8d0e8f3a3f9383c
Joey Huab <joey@evolution-x.org>
2022-04-12 05:28:14 +0900

    PixelPropsUtils: Remove Fortnite remnant
    Change-Id: I37885a2f840d80f31bfda5f9db0c32a92e741223

--- core/java/com/android/internal/util/dot/PixelPropsUtils.java
+++ core/java/com/android/internal/util/dot/PixelPropsUtils.java
@@ -101,8 +101,6 @@ public class PixelPropsUtils {
             "com.riotgames.league.wildrifttw",
             "com.riotgames.league.wildriftvn",
             "com.netease.lztgglobal",
-            "com.epicgames.fortnite",
-            "com.epicgames.portal"
     };
 
     private static final String[] packagesToChangeMI11 = {
