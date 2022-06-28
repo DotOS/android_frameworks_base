83d3938147470f0a3e1054ca6784f57f6aae3679
LibXZR <i@xzr.moe>
2022-06-28 18:42:23 +0800

    display: Restore brightness adjustment on boot
    Well, it now remembers what learnt before.
    
    Change-Id: Ic08771900d73e95d285a0f303d8155825252a595
    Signed-off-by: LibXZR <i@xzr.moe>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -913,7 +913,8 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
             return;
         }
 
-        mBrightnessMapper = BrightnessMappingStrategy.create(resources, mDisplayDeviceConfig);
+        mBrightnessMapper = BrightnessMappingStrategy.create(
+                resources, mDisplayDeviceConfig, getAutoBrightnessAdjustmentSetting());
 
         if (mBrightnessMapper != null) {
             final float dozeScaleFactor = resources.getFraction(
